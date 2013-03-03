0064210ad1db4ddab80a334601892900a99595b7
fibo <casati_gianluca@yahoo.it>
2013-03-03 12:46:57 +0100

    added Octahedron and Tetrahedron nodes

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Geometry/Primitives/DX11TetrahedronNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.PluginInterfaces.V1;
+
+using SlimDX.Direct3D11;
+using SlimDX;
+
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "Tetrahedron", Category = "DX11.Geometry", Version = "", Author = "fibo")]
+    public class DX11TetrahedronNode : DX11BasePrimitiveNode
+    {
+        /*[Input("Size",DefaultValues= new double[] { 1,1,1})]
+        IDiffSpread<Vector3> FSize;*/
+
+        protected override DX11IndexedGeometry GetGeom(DX11RenderContext context, int slice)
+        {
+            return context.Primitives.Tetrahedron(new Vector3(1, 1, 1));
+        }
+
+        protected override bool Invalidate()
+        {
+            return false;// this.FSize.IsChanged;
+        }
+    }
+}
