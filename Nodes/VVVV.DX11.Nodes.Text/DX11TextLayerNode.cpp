5ce8683969c492c705d2b5e5825600608b72fff8
mrvux <julien@mrvux.com>
2013-03-04 12:30:24 +0100

    -Import text

---
+++ Nodes/VVVV.DX11.Nodes.Text/DX11TextLayerNode.cpp
@@ -0,0 +1,141 @@
+#include "StdAfx.h"
+#include "DX11TextLayerNode.h"
+
+namespace VVVV { namespace Nodes { namespace DX11 {
+
+DX11TextLayerNode::DX11TextLayerNode(IIOFactory^ factory)
+{
+	this->iofactory = factory;
+	factory->PluginHost->CreateTransformInput("Transform In",TSliceMode::Dynamic,TPinVisibility::True,this->FInTr);
+	this->fontrenderers = gcnew	Dictionary<DX11RenderContext^,IntPtr>();
+}
+
+void DX11TextLayerNode::Evaluate(int SpreadMax)
+{
+	this->spmax = SpreadMax;
+
+	if (this->FOutLayer[0] == nullptr)
+    {
+		this->FOutLayer[0] = gcnew DX11Resource<DX11Layer^>();
+	}
+}
+
+void DX11TextLayerNode::Update(IPluginIO^ pin, DX11RenderContext^ context)
+{
+    if (!this->FOutLayer[0]->Contains(context))
+    {
+		this->FOutLayer[0][context] = gcnew DX11Layer();
+        this->FOutLayer[0][context]->Render = gcnew RenderDelegate<DX11RenderSettings^>(this,&DX11TextLayerNode::Render);
+    }
+
+	if (!this->fontrenderers->ContainsKey(context))
+	{
+		FW1_FONTWRAPPERCREATEPARAMS createParams = {0};
+		createParams.SheetMipLevels = 5;
+		createParams.AnisotropicFiltering = TRUE;
+		createParams.DefaultFontParams.pszFontFamily = L"Arial";
+		createParams.DefaultFontParams.FontWeight = DWRITE_FONT_WEIGHT_NORMAL;
+		createParams.DefaultFontParams.FontStyle = DWRITE_FONT_STYLE_NORMAL;
+		createParams.DefaultFontParams.FontStretch = DWRITE_FONT_STRETCH_NORMAL;
+
+
+		IFW1Factory *pFW1Factory;
+		FW1CreateFactory(FW1_VERSION, &pFW1Factory);
+		ID3D11Device* dev = (ID3D11Device*)context->Device->ComPointer.ToPointer();
+		
+		IFW1FontWrapper* pw;
+		
+		pFW1Factory->CreateFontWrapper(dev, NULL,&createParams, &pw);	
+		pFW1Factory->Release();
+
+		this->fontrenderers->Add(context,IntPtr(pw));
+	}
+}
+
+
+void DX11TextLayerNode::Destroy(IPluginIO^ pin, DX11RenderContext^ context, bool force)
+{
+    if (this->FOutLayer[0]->Contains(context))
+    {
+		this->FOutLayer[0]->Dispose(context);
+	}
+}
+
+void DX11TextLayerNode::Render(IPluginIO^ pin,DX11RenderContext^ context, DX11RenderSettings^ settings)
+{
+	if (this->FInEnabled[0])
+	{
+		float w = settings->RenderWidth;
+		float h = settings->RenderHeight;
+
+		IFW1FontWrapper* fw = (IFW1FontWrapper*)this->fontrenderers[context].ToPointer();
+
+		ID3D11Device* dev = (ID3D11Device*)context->Device->ComPointer.ToPointer();
+		ID3D11DeviceContext* pContext = (ID3D11DeviceContext*)context->CurrentDeviceContext->ComPointer.ToPointer();
+
+
+		int cnt;
+		float* tr;
+		this->FInTr->GetMatrixPointer(cnt,tr);
+
+		SlimDX::Matrix* smp = (SlimDX::Matrix*)&tr[0];
+
+		for (int i = 0; i < this->spmax; i++)
+		{
+
+			void* txt = (void*)Marshal::StringToHGlobalUni(this->FInString[i]);
+			void* font = (void*)Marshal::StringToHGlobalUni(this->FFontInput[i]);
+			float size = this->FInSize[i];
+			int color = this->FInColor[i].Color.ToArgb();
+
+
+			SlimDX::Matrix preScale = SlimDX::Matrix::Scaling(1.0f,-1.0f,1.0f);
+			switch (this->FNormalizeInput[i]->Index)
+			{
+			case 1: preScale = SlimDX::Matrix::Scaling(1.0f / w, -1.0f / w, 1.0f); break;
+			case 2: preScale = SlimDX::Matrix::Scaling(1.0f / h, -1.0f / h, 1.0f); break;
+			case 3: preScale = SlimDX::Matrix::Scaling(1.0f / w, -1.0f / h, 1.0f); break;
+			}
+
+			SlimDX::Matrix sm = smp[i % this->FInTr->SliceCount];
+
+			SlimDX::Matrix mat = SlimDX::Matrix::Multiply(preScale,sm);
+			mat = SlimDX::Matrix::Multiply(mat,settings->View);
+			mat = SlimDX::Matrix::Multiply(mat,settings->Projection);
+
+			float* tr = (float*)&mat;
+
+			FW1_RECTF rect = {0.0f, 0.0f, 0.0f, 0.0f};
+
+			int flag = FW1_NOWORDWRAP;
+
+			if (this->FHorizontalAlignInput[i]->Index == 0) { flag |= FW1_LEFT; }
+			else if (this->FHorizontalAlignInput[i]->Index == 1) { flag |= FW1_CENTER; }
+			else if (this->FHorizontalAlignInput[i]->Index == 2) { flag |= FW1_RIGHT; }
+
+			if (this->FVerticalAlignInput[i]->Index == 0) { flag |= FW1_TOP; }
+			else if (this->FVerticalAlignInput[i]->Index == 1) { flag |= FW1_VCENTER; }
+			else if (this->FVerticalAlignInput[i]->Index == 2) { flag |= FW1_BOTTOM; }
+
+			fw->DrawString(
+					pContext,
+					(WCHAR*)txt,
+					(WCHAR*)font,
+					size,
+					&rect,
+					color,
+					NULL, 
+					tr,
+					flag
+				);
+
+			Marshal::FreeHGlobal(System::IntPtr(txt));
+			Marshal::FreeHGlobal(System::IntPtr(font));
+		}
+
+		//Apply old states back
+		context->RenderStateStack->Apply();
+	}
+}
+
+}}}
---
+++ Nodes/VVVV.DX11.Nodes.Text/DX11TextLayerNode.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "FW1FontWrapper.h"
+#include <map>
+
+using namespace VVVV::PluginInterfaces::V2;
+using namespace VVVV::PluginInterfaces::V1;
+
+using namespace FeralTic::DX11;
+using namespace FeralTic::DX11::Resources;
+
+using namespace VVVV::DX11;
+
+using namespace System::Runtime::InteropServices;
+using namespace System::Collections::Generic;
+using namespace System;
+using namespace System::ComponentModel::Composition;
+
+namespace VVVV { namespace Nodes { namespace DX11 {
+
+[PluginInfo(Name="Text",Author="vux",Category="DX11.Layer",Version="")]
+public ref class DX11TextLayerNode : public IPluginEvaluate,IDX11LayerProvider
+{
+public:
+	[ImportingConstructor()]
+	DX11TextLayerNode(IIOFactory^ factory);
+	virtual void Evaluate(int SpreadMax);
+	virtual void Update(IPluginIO^ pin, DX11RenderContext^ OnDevice);
+	virtual void Destroy(IPluginIO^ pin, DX11RenderContext^ OnDevice, bool force);
+private:
+	ITransformIn^ FInTr;
+
+	[Input("String",DefaultString="DX11")]
+    ISpread<System::String^>^ FInString;
+
+	[Input("Font", EnumName = "SystemFonts")]
+	ISpread<EnumEntry^>^ FFontInput;
+
+	[Input("Italic")]
+	ISpread<bool>^ FItalicInput;
+
+	[Input("Bold")]
+	IDiffSpread<bool>^ FBoldInput;
+
+	[Input("Size")]
+    ISpread<float>^ FInSize;
+
+	[Input("Color")]
+    ISpread<VVVV::Utils::VColor::RGBAColor>^ FInColor;
+
+	[Input("Horizontal Align", EnumName = "HorizontalAlign")]
+	ISpread<EnumEntry^>^ FHorizontalAlignInput;
+
+	[Input("Vertical Align", EnumName = "VerticalAlign")]
+	ISpread<EnumEntry^>^ FVerticalAlignInput;
+
+	[Input("Normalize", EnumName = "Normalize")]
+	ISpread<EnumEntry^>^ FNormalizeInput;
+
+	[Input("Enabled",IsSingle = true,DefaultValue=1)]
+	ISpread<bool>^ FInEnabled;
+
+
+	[Output("Layer", IsSingle=true)]
+    ISpread<DX11Resource<DX11Layer^>^>^ FOutLayer;
+
+	void Render(IPluginIO^ pin,DX11RenderContext^ ctx, DX11RenderSettings^ settings);
+
+	Dictionary<DX11RenderContext^,IntPtr>^ fontrenderers;
+	int spmax;
+
+	IIOFactory^ iofactory;
+};
+
+}}}
