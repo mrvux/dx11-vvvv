e5348b4876c3e90084fa1dd081f7f153a5fd6c5f
mrvux <julien@mrvux.com>
2013-07-05 12:29:08 +0200

    -Added Aspect Ratio Layer version

--- Nodes/VVVV.DX11.Nodes/Nodes/Layers/Space/WithinLayerNodes.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Layers/Space/WithinLayerNodes.cs
@@ -71,4 +71,66 @@ namespace VVVV.DX11.Nodes
             settings.ViewProjection = settings.Projection;
         }
     }
+
+    [PluginInfo(Name = "AspectRatio", Category = "DX11.Layer", Version = "")]
+    public class AspectRatioNode : AbstractDX11LayerSpaceNode
+    {
+        [Input("Uniform Scale", DefaultValue=1, IsSingle = true)]
+        ISpread<float> FScale;
+
+        [Input("Alignment", DefaultEnumEntry = "FitIn", EnumName = "AspectRatioAlignment", IsSingle = true)]
+        ISpread<EnumEntry> FAlign;
+
+        protected override void UpdateSettings(DX11RenderSettings settings)
+        {
+            float w = settings.RenderWidth;
+            float h = settings.RenderHeight;
+
+            float sx, sy;
+
+            if (FAlign[0].Name == "FitIn")
+            {
+                if (w > h)
+                {
+                    sx = h / w;
+                    sy = 1.0f;
+                }
+                else
+                {
+                    sx = 1.0f;
+                    sy = w / h;
+                }
+            }
+            else if (FAlign[0].Name == "FitOut")
+            {
+                if (w > h)
+                {
+                    sx = 1.0f;
+                    sy = w / h;
+                }
+                else
+                {
+                    sx = h / w;
+                    sy = 1.0f;
+                }
+            }
+            else if (FAlign[0].Name == "FitWidth")
+            {
+                sx = 1.0f;
+                sy = w / h;
+            }
+            else //FitHeight
+            {
+                sy = 1.0f;
+                sx = h / w;
+            }
+
+            Matrix aspect = Matrix.Scaling(sx* FScale[0], sy * FScale[0], 1.0f);
+           
+            settings.Projection = settings.Projection * aspect;
+            settings.ViewProjection = settings.View * settings.Projection;
+        }
+    }
+
+    
 }
