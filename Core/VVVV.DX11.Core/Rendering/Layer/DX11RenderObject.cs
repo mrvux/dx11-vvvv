2a92d9cb761ce39dc05388d0a205b37f87ba904c
mrvux <julien@mrvux.com>
2013-08-18 18:22:26 +0200

    Added Render Object

---
+++ Core/VVVV.DX11.Core/Rendering/Layer/DX11RenderObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX;
+
+namespace VVVV.DX11
+{
+    public class DX11RenderObject
+    {
+        public string ObjectType { get; set; }
+
+        public object Descriptor { get; set; }
+
+        public Matrix[] Transforms { get; set; }
+
+    }
+
+    public class DX11ObjectGroup
+    {
+        public DX11ObjectGroup()
+        {
+            this.RenderObjects = new List<DX11RenderObject>();
+            this.Semantics = new List<IDX11RenderSemantic>();
+        }
+
+        public string ShaderName { get; set; }
+
+        public List<DX11RenderObject> RenderObjects { get; set; }
+
+        public List<IDX11RenderSemantic> Semantics { get; set; }
+    }
+}
