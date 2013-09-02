eb41a9e8cab9c9c70c0129fa9865a82bf2c472f8
mrvux <julien@mrvux.com>
2013-09-02 11:02:15 +0200

    Removed enum in back buffer format for windowed renderer

--- Nodes/VVVV.DX11.Nodes/Nodes/Renderers/Graphics/DX11RendererNode.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Renderers/Graphics/DX11RendererNode.cs
@@ -218,10 +218,6 @@ namespace VVVV.DX11.Nodes
         string oldbbformat = "";
         #endregion
 
-        #region Config Pins
-        IDiffSpread<EnumEntry> FCfgBackBufferFormat;
-        #endregion
-
         #region Output Pins
         [Output("Mouse State",AllowFeedback=true)]
         protected ISpread<MouseState> FOutMouseState;
@@ -301,9 +297,8 @@ namespace VVVV.DX11.Nodes
                 this.depthmanager.FormatChanged = false; //Clear flag ok
             }
             
-            if (this.oldbbformat != this.FCfgBackBufferFormat[0].Name || FInAASamplesPerPixel.IsChanged)
+            if (FInAASamplesPerPixel.IsChanged)
             {
-                this.oldbbformat = this.FCfgBackBufferFormat[0];
                 this.depthmanager.NeedReset = true;
                 this.FInvalidateSwapChain = true;
             }
@@ -503,14 +498,9 @@ namespace VVVV.DX11.Nodes
 
             if (this.FResized || this.FInvalidateSwapChain || this.FOutBackBuffer[0][context] == null)
             {
-                EnumEntry bbf = this.FCfgBackBufferFormat[0];
-
                 this.FOutBackBuffer[0].Dispose(context);
 
-                //NOTE ENUM BROKEN
-                Format fmt = (Format)Enum.Parse(typeof(Format), this.FCfgBackBufferFormat[0].Name);
-
-                this.FOutBackBuffer[0][context] = new DX11SwapChain(context,this.Handle, fmt, sd);
+                this.FOutBackBuffer[0][context] = new DX11SwapChain(context,this.Handle, Format.R8G8B8A8_UNorm, sd);
                 #if DEBUG
                 this.FOutBackBuffer[0][context].Resource.DebugName = "BackBuffer";
                 #endif
