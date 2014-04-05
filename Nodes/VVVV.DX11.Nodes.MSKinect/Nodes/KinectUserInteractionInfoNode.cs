8cb115e834172fb6804e7602774655542f1594a1
Julien Vulliet <julien@mrvux.com>
2014-04-05 12:03:32 +0200

    Kinect has nodes for user interaction (aka: grip)

---
+++ Nodes/VVVV.DX11.Nodes.MSKinect/Nodes/KinectUserInteractionInfoNode.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using Microsoft.Kinect.Toolkit.Interaction;
+using SlimDX;
+using VVVV.Utils.VMath;
+
+namespace VVVV.DX11.Nodes.Nodes
+{
+    [PluginInfo(Name = "UserInfo", Category = "Kinect", Version = "Microsoft", Author = "vux")]
+    public class KinectUserInteractionInfoNode : IPluginEvaluate
+    {
+        [Input("User Info")]
+        private Pin<UserInfo> FInUI;
+
+        [Output("Hand Count")]
+        private ISpread<int> FOutHandCount;
+
+        [Output("Is Tracked")]
+        private ISpread<bool> FOutIsTracked;
+
+        [Output("Is Pressed")]
+        private ISpread<bool> FOutIsPressed;
+
+        [Output("Position")]
+        private ISpread<Vector2D> FOutHandPos;
+
+        [Output("Press Extent")]
+        private ISpread<double> FOutExtent;
+
+        [Output("Hand Type")]
+        private ISpread<string> FOutHandType;
+
+        [Output("Hand Interaction Type")]
+        private ISpread<string> FOutHandInterType;
+
+
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FInUI.PluginIO.IsConnected)
+            {
+                this.FOutHandPos.SliceCount = this.FInUI.SliceCount*2;
+                this.FOutHandType.SliceCount = this.FInUI.SliceCount * 2;
+                this.FOutHandInterType.SliceCount = this.FInUI.SliceCount * 2;
+                this.FOutIsTracked.SliceCount = this.FInUI.SliceCount * 2;
+                this.FOutIsPressed.SliceCount = this.FInUI.SliceCount * 2;
+                this.FOutExtent.SliceCount = this.FInUI.SliceCount * 2;
+
+                this.FOutHandCount.SliceCount = this.FInUI.SliceCount;
+
+                int cnt = 0;
+                for (int i = 0; i < this.FInUI.SliceCount; i++)
+                {
+                    UserInfo ui = this.FInUI[i];
+                    this.FOutHandCount[i] = ui.HandPointers.Count;
+
+                    for (int j = 0 ; j < ui.HandPointers.Count;j++)
+                    {
+                        this.FOutHandPos[cnt] = new Vector2D(ui.HandPointers[j].X, ui.HandPointers[j].Y);
+                        this.FOutHandType[cnt] = ui.HandPointers[j].HandType.ToString();
+                        this.FOutHandInterType[cnt] = ui.HandPointers[j].HandEventType.ToString();
+
+                        this.FOutExtent[cnt] = ui.HandPointers[j].PressExtent;
+                        this.FOutIsPressed[cnt] = ui.HandPointers[j].IsPressed;
+                        this.FOutIsTracked[cnt] = ui.HandPointers[j].IsTracked;
+
+                        cnt++;
+                    }
+
+                }
+            }
+            else
+            {
+                this.FOutHandPos.SliceCount = 0;
+                this.FOutHandCount.SliceCount = 0;
+                this.FOutHandType.SliceCount = 0;
+                this.FOutHandInterType.SliceCount = 0;
+                this.FOutIsTracked.SliceCount = 0;
+                this.FOutIsPressed.SliceCount = 0;
+                this.FOutExtent.SliceCount = 0;
+            }
+        }
+    }
+}
