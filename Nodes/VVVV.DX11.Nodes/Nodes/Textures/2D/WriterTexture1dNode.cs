53bcb066004c03d0be3c555ec3d5e9083ce372f6
Julien Vulliet <julien@mrvux.com>
2014-07-18 14:18:32 +0200

    Add writer for 1d texture
    Add advanced buffer renderer, to allow counter buffers as well
    Texture array renderer has option to bind whole target now
    Volume renderer allows to bind as rendertarget

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/WriterTexture1dNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using SlimDX.Direct3D11;
+
+using VVVV.Core.Logging;
+
+using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "Writer", Category = "DX11.Texture", Version = "1d", Author = "vux", AutoEvaluate = true)]
+    public class WriterTexture1DNode : IPluginEvaluate, IDX11ResourceDataRetriever
+    {
+        [Input("Texture In")]
+        protected Pin<DX11Resource<DX11Texture2D>> FTextureIn;
+
+        [Input("Filename",StringType=StringType.Filename,DefaultString="render")]
+        protected ISpread<string> FInPath;
+
+        [Input("Write", IsBang = true)]
+        protected ISpread<bool> FInSave;
+
+        [Output("Valid")]
+        protected ISpread<bool> FOutValid;
+
+        [Import()]
+        protected IPluginHost FHost;
+
+        [Import()]
+        protected ILogger FLogger;
+
+        public DX11RenderContext AssignedContext
+        {
+            get;
+            set;
+        }
+
+        public event DX11RenderRequestDelegate RenderRequest;
+
+
+        #region IPluginEvaluate Members
+
+        public void Evaluate(int SpreadMax)
+        {
+            this.FOutValid.SliceCount = 1;
+
+            if (this.FTextureIn.PluginIO.IsConnected)
+            {
+                if (this.RenderRequest != null) { this.RenderRequest(this, this.FHost); }
+
+                if (this.AssignedContext == null) { this.FOutValid.SliceCount = 0; return; }
+                //Do NOT cache this, assignment done by the host
+
+                for (int i = 0; i < SpreadMax; i++)
+                {
+                    if (this.FTextureIn[i].Contains(this.AssignedContext) && this.FInSave[i])
+                    {
+                        try
+                        {
+                            string path = this.FInPath[i];
+                            if (!path.EndsWith(".dds",StringComparison.OrdinalIgnoreCase))
+                            {
+                                path += ".dds";
+                            }
+                            Texture1D.SaveTextureToFile(this.AssignedContext.CurrentDeviceContext, this.FTextureIn[i][this.AssignedContext].Resource, ImageFileFormat.Dds, path);
+                            this.FOutValid[0] = true;
+                        }
+                        catch (Exception ex)
+                        {
+                            FLogger.Log(ex);
+                            this.FOutValid[0] = false;
+                        }
+                    }
+                    else
+                    {
+                        this.FOutValid[0] = false;
+                    }
+                }
+            }
+            else
+            {
+                this.FOutValid.SliceCount = 0;
+
+            }
+        }
+
+        #endregion
+    }
+}
