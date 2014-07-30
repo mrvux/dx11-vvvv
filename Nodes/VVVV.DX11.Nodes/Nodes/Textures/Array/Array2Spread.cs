ac7efdee0ad65c42dc89346c0a1bd85702db6a12
sebl <sebl@schnellebuntebilder.de>
2014-07-30 16:50:58 +0200

    array2spread progress. CopySubresourceRegion leads to an exception

--- Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/Array2Spread.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/Array2Spread.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.Composition;
 
 using SlimDX;
 using SlimDX.Direct3D11;
+using SlimDX.DXGI;
 
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
@@ -24,65 +25,101 @@ namespace VVVV.DX11.Nodes
     public class Array2Spread : IPluginEvaluate, IDX11ResourceProvider, IDisposable
     {
         [Input("TextureArray In", IsSingle = true)]
-        protected Pin<DX11Resource<DX11TextureArray2D>> FTexIn;
+        //protected Pin<DX11Resource<DX11TextureArray2D>> FTexIn;
+        protected Pin<DX11Resource<DX11RenderTextureArray>> FTexIn;
 
-        [Input("Width", IsSingle = true, DefaultValue = 512)]
-        protected ISpread<int> Width;
+        [Input("Element Count", DefaultValue = 1)]
+        protected IDiffSpread<int> FInElementCount;
 
-        [Input("Height", IsSingle = true, DefaultValue = 512)]
-        protected ISpread<int> Height;
-
-        [Input("Depth", IsSingle = true, DefaultValue = 4)]
-        protected ISpread<int> Depth;
-
-        [Input("Format", IsSingle = true, DefaultEnumEntry = "Unknown")]
-        protected ISpread<SlimDX.DXGI.Format> Format;
-
-        [Input("Slice Index")]
-        protected ISpread<int> FSliceIndex;
-
-        [Input("Reset", IsBang = true)]
-        protected ISpread<bool> FReset;
-
-        [Input("Write", IsBang = true)]
-        protected ISpread<bool> FWrite;
+        //[Input("Reset", IsBang = true)]
+        //protected ISpread<bool> FReset;
 
         [Output("Textures Out")]
         protected ISpread<DX11Resource<DX11Texture2D>> FTextureOutput;
 
-        public DX11RenderContext AssignedContext
-        {
-            get;
-            set;
-        }
 
 
         public void Evaluate(int SpreadMax)
         {
+            FTextureOutput.SliceCount = FInElementCount[0];
 
-            int ArrayCount = FTexIn.SliceCount; // no no no
+            for (int i = 0; i < FInElementCount[0]; i++)
+            {
+                if (this.FTextureOutput[i] == null)
+                {
+                    this.FTextureOutput[i] = new DX11Resource<DX11Texture2D>();
+                }
+               
+            }
 
-            FTextureOutput.SliceCount = FTexIn.SliceCount;
+        }
+        
+        public void Update(IPluginIO pin, DX11RenderContext context)
+        {
+            if (this.FTextureOutput.SliceCount == 0) { return; }
+
+            int ArrayCount = FTexIn[0][context].ElemCnt;
 
             for (int i = 0; i < ArrayCount; i++)
             {
-                //IDX11Resource b= this.FTexIn[this.AssignedContext];
+                //if (!this.FTextureOutput[i].Contains(context))
+                //{
+                Texture2DDescription desc;
 
-                //this.AssignedContext.CurrentDeviceContext.CopySubresourceRegion()
+                    if (this.FTextureOutput[i].Contains(context))
+                    {
+                        desc = FTexIn[0][context].Resource.Description;
 
-                if (this.FTextureOutput[i] == null)
-                {
+                        if (desc.Width != this.FTexIn[0][context].Resource.Description.Width || desc.Height != this.FTexIn[0][context].Resource.Description.Height || desc.Format != this.FTexIn[0][context].Resource.Description.Format)
+                        {
+                            this.FTextureOutput[i].Dispose(context);
+                            this.FTextureOutput[i] = new DX11Resource<DX11Texture2D>();
+                        }
+                    }
+                    else
+                    {
+                        this.FTextureOutput[i][context] = new DX11Texture2D();
+                    }
 
-                    this.FTextureOutput[i] = new DX11Resource<DX11Texture2D>( );
-                }
+                    //desc = this.FTextureOutput[i][context].Resource.Description;
 
-                this.FTextureOutput[i] = FTexIn.
+                    desc = this.FTexIn[0][context].Resource.Description;
+
+                    SlimDX.Direct3D11.Resource source = this.FTexIn[0][context].Resource;
+                    SlimDX.Direct3D11.Resource destiantion = this.FTextureOutput[i][context].Resource;
+
+                    // here's the error
+                    context.CurrentDeviceContext.CopySubresourceRegion(source, i, destiantion, 0, 0, 0, 0);
+
+                //}
+                //else // contains context
+                //{
+                //    IDX11ResourceProvider provider = source.Instance<IDX11ResourceProvider>();
+                //    this.FTextureOutput[i].Assign(context.);
+                //}
             }
+        }
+        
 
-
+        public void Dispose()
+        {
+            if (this.FTextureOutput.SliceCount > 0)
+            {
+                if (this.FTextureOutput[0] != null)
+                {
+                    this.FTextureOutput[0].Dispose();
+                }
+            }
         }
 
-        
+
+        public void Destroy(IPluginIO pin, DX11RenderContext context, bool force)
+        {
+            for (int i = 0; i < FTextureOutput.SliceCount; i++ )
+            {
+                this.FTextureOutput[i].Dispose(context);
+            }
+        }
 
 
     }
