83af866ed6a71fb6e18b2792cf47d2d0159e90cd
Julien Vulliet <julien@mrvux.com>
2014-12-26 12:09:49 +0100

    Assimp geometry split node
    Compressed writer

---
+++ Nodes/VVVV.DX11.Nodes.Assimp/AssimpMeshSplitNode.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using VVVV.PluginInterfaces.V1;
+using AssimpNet;
+using System.ComponentModel.Composition;
+using SlimDX;
+using SlimDX.Direct3D11;
+
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+using System.Runtime.InteropServices;
+
+namespace VVVV.DX11.Nodes.Assimp
+{
+    [PluginInfo(Name="Mesh",Category="Geometry Split",Version="Assimp",Author="vux,flateric")]
+    public unsafe class AssimpMeshSplitNode : IPluginEvaluate
+    {
+        [DllImport("msvcrt.dll", EntryPoint = "memcpy", CallingConvention = CallingConvention.Cdecl, SetLastError = false)]
+        private static extern IntPtr memcpy(IntPtr dest, IntPtr src, int count);
+
+        [Input("Scene",IsSingle=true)]
+        protected IDiffSpread<AssimpScene> scene;
+
+        [Input("Mesh Index", IsSingle = true)]
+        protected IDiffSpread<int> meshindex;
+
+        [Output("Position", Order = 5, BinName="Vertices Count", BinOrder=4)]
+        protected ISpread<ISpread<Vector3>> position;
+
+        [Output("Normals", Order = 6, BinVisibility=PinVisibility.OnlyInspector)]
+        protected ISpread<ISpread<Vector3>> normals;
+
+        [Output("UV Channel 1", Order = 7, BinVisibility = PinVisibility.OnlyInspector)]
+        protected ISpread<ISpread<float>> uv1;
+
+        [Output("UV Channel Count", Order = 10, BinVisibility = PinVisibility.OnlyInspector)]
+        protected ISpread<int> uvchancount;
+
+        [Output("Indices", Order = 15, BinName="Indices Count", BinOrder=16)]
+        protected ISpread<ISpread<int>> indices;
+
+        private bool FInvalidate = false;
+
+
+        [ImportingConstructor()]
+        public AssimpMeshSplitNode(IPluginHost host)
+        {
+        }
+
+        public void Evaluate(int SpreadMax)
+        { 
+            if (this.scene.IsChanged || this.meshindex.IsChanged)
+            {
+                if (this.scene[0] != null)
+                {
+                    int meshcnt = this.scene[0].MeshCount;
+
+                    if (this.meshindex[0] < 0)
+                    {
+                        this.position.SliceCount = meshcnt;
+                        this.normals.SliceCount = meshcnt;
+                        this.uv1.SliceCount = meshcnt;
+                        this.uvchancount.SliceCount = meshcnt;
+                        this.indices.SliceCount = meshcnt;
+
+                        for (int i = 0; i < meshcnt; i++)
+                        {
+                            this.WriteMesh(this.scene[0].Meshes[i], i);
+                        }
+                    }
+                    else
+                    {
+                        int meshidx = this.meshindex[0] % this.scene[0].MeshCount;
+
+                        this.position.SliceCount = 1;
+                        this.normals.SliceCount = 1;
+                        this.uv1.SliceCount = 1;
+                        this.uvchancount.SliceCount = 1;
+                        this.indices.SliceCount = 1;
+
+                        this.WriteMesh(this.scene[0].Meshes[meshidx], 0);
+                    }
+                }
+                else
+                {
+                    this.position.SliceCount = 0;
+                    this.normals.SliceCount = 0;
+                    this.uv1.SliceCount = 0;
+                    this.uvchancount.SliceCount = 0;
+                    this.indices.SliceCount = 0;
+                }
+
+                this.position.Flush(true);
+                this.normals.Flush(true);
+                this.uv1.Flush(true);
+                this.indices.Flush(true);
+            }
+        }
+
+        private void WriteMesh(AssimpMesh mesh, int slice)
+        {
+            this.position[slice].SliceCount = mesh.VerticesCount;
+
+            fixed (Vector3* vptr = &this.position[slice].Stream.Buffer[0])
+            {
+                memcpy(new IntPtr(vptr), mesh.PositionPointer, mesh.VerticesCount * 12);
+            }
+
+            if (mesh.HasNormals)
+            {
+                this.normals[slice].SliceCount = mesh.VerticesCount;
+                fixed (Vector3* nptr = &this.normals[slice].Stream.Buffer[0])
+                {
+                    memcpy(new IntPtr(nptr), mesh.NormalsPointer, mesh.VerticesCount * 12);
+                }
+            }
+            else
+            {
+                this.normals[slice].SliceCount = 0;
+            }
+
+            if (mesh.UvChannelCount > 0)
+            {
+                int chancnt = this.GetChannelCount(mesh, 0);
+                this.uv1[slice].SliceCount = mesh.VerticesCount * chancnt;
+                this.uvchancount[slice] = chancnt;
+
+                fixed (float* uptr = &this.uv1[slice].Stream.Buffer[0])
+                {
+                    memcpy(new IntPtr(uptr), mesh.GetUvPointer(0), mesh.VerticesCount * 4 * chancnt);
+                }
+            }
+            else
+            {
+                this.uv1[slice].SliceCount = 0;
+                this.uvchancount[slice] = 0;
+            }
+
+            this.indices[slice].SliceCount = mesh.Indices.Count;
+            var inds = mesh.Indices;
+            var ibo = this.indices[slice].Stream.Buffer;
+            for (int i = 0; i < mesh.Indices.Count; i++)
+            {
+                ibo[i] = inds[i];
+            }
+        }
+
+        private int GetChannelCount(AssimpMesh mesh, int slot)
+        {
+            var fmt = mesh.GetInputElements().Where(ie => ie.SemanticName == "TEXCOORD" && ie.SemanticIndex == slot).First();
+
+            switch(fmt.Format)
+            {
+                case SlimDX.DXGI.Format.R32_Float:
+                    return 1;
+                case SlimDX.DXGI.Format.R32G32_Float:
+                    return 2;
+                case SlimDX.DXGI.Format.R32G32B32_Float:
+                    return 3;
+                case SlimDX.DXGI.Format.R32G32B32A32_Float:
+                    return 4;
+                default :
+                    return 0;
+            }
+
+        }
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/TextureLoader.cs
@@ -0,0 +1,219 @@
+﻿using SlimDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeralTic.DX11.Resources
+{
+
+    public enum eImageFormat { Bmp = 1, Jpeg = 2, Png = 3, Tiff = 4, Gif = 5, Hdp = 6,Dds = 128, Tga = 129 }
+
+    public enum DdsBlockType
+    {
+        BC1 = SlimDX.DXGI.Format.BC1_UNorm,
+        BC2 = SlimDX.DXGI.Format.BC2_UNorm,
+        BC3 = SlimDX.DXGI.Format.BC3_UNorm,
+        BC7 = SlimDX.DXGI.Format.BC7_UNorm,
+        BC6S = SlimDX.DXGI.Format.BC6_SFloat16,
+        BC6U = SlimDX.DXGI.Format.BC6_UFloat16,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ImageMetadata
+    {
+        public long Width;
+        public long Height;
+        public long Depth;
+        public long ArraySize;
+        public long MipLevels;
+        public int MiscFlags;
+        public int MiscFlags2;
+        public SlimDX.DXGI.Format Format;
+        public SlimDX.Direct3D11.ResourceDimension Dimension;
+    }
+
+
+    public class TextureLoader
+    {
+        public class NativeMethods
+        {
+            public static ImageMetadata LoadMetadataFromFile(string path)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.LoadMetadataFromFile(path);
+                }
+                else
+                {
+                    return NativeMethods32.LoadMetadataFromFile(path);
+                }
+            }
+
+            public static ImageMetadata LoadMetadataFromMemory(IntPtr dataPointer, int dataLength)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.LoadMetadataFromMemory(dataPointer, dataLength);
+                }
+                else
+                {
+                    return NativeMethods32.LoadMetadataFromMemory(dataPointer, dataLength);
+                }
+            }
+
+            public static long LoadTextureFromFile(IntPtr device, string path,out IntPtr resource, int miplevels)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.LoadTextureFromFile(device, path, out resource, miplevels);
+                }
+                else
+                {
+                    return NativeMethods32.LoadTextureFromFile(device, path, out resource, miplevels);
+                }
+            }
+
+            public static long SaveTextureToFile(IntPtr device, IntPtr context, IntPtr resource, string path, int format)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.SaveTextureToFile(device, context,resource, path, format);
+                }
+                else
+                {
+                    return NativeMethods32.SaveTextureToFile(device, context, resource, path, format);
+                }
+            }
+
+            public static long SaveCompressedTextureToFile(IntPtr device, IntPtr context, IntPtr resource, string path, int blocktype)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.SaveCompressedTextureToFile(device, context, resource, path, blocktype);
+                }
+                else
+                {
+                    return NativeMethods32.SaveCompressedTextureToFile(device, context, resource, path, blocktype);
+                }
+            }
+
+            public static long LoadTextureFromMemory(IntPtr device, IntPtr dataPointer, int dataLength, out IntPtr resource)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.LoadTextureFromMemory(device, dataPointer, dataLength, out resource);
+                }
+                else
+                {
+                    return NativeMethods32.LoadTextureFromMemory(device, dataPointer, dataLength, out resource);
+                }
+            }
+
+            public static long SaveCompressedTextureToMemory(IntPtr device, IntPtr context, IntPtr resource, int blocktype, out IntPtr data, out int size, out IntPtr blobPtr)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    return NativeMethods64.SaveCompressedTextureToMemory(device, context, resource, blocktype, out data, out size, out blobPtr);
+                }
+                else
+                {
+                    return NativeMethods32.SaveCompressedTextureToMemory(device, context, resource, blocktype, out data, out size, out blobPtr);
+                }
+            }
+
+            public static void DeleteBlob(IntPtr blob)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    NativeMethods64.DeleteBlob(blob);
+                }
+                else
+                {
+                    NativeMethods32.DeleteBlob(blob);
+                }
+            }
+        }
+
+        private class NativeMethods64
+        {
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern ImageMetadata LoadMetadataFromFile(string path);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern ImageMetadata LoadMetadataFromMemory(IntPtr dataPointer, int dataLength);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern long LoadTextureFromFile(IntPtr device, string path, out IntPtr resource, int miplevels);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern long LoadTextureFromMemory(IntPtr device, IntPtr dataPointer, int dataLength, out IntPtr resource);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern long SaveTextureToFile(IntPtr device, IntPtr context, IntPtr resource, string path, int format);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern long SaveCompressedTextureToFile(IntPtr device, IntPtr context, IntPtr resource, string path, int blocktype);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern long SaveCompressedTextureToMemory(IntPtr device, IntPtr context, IntPtr resource, int blocktype, out IntPtr data, out int size, out IntPtr blobPtr);
+
+            [DllImport("DirectXTexLib_x64", CharSet = CharSet.Unicode)]
+            public static extern void DeleteBlob(IntPtr blob);
+        }
+
+
+        private class NativeMethods32
+        {
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern ImageMetadata LoadMetadataFromFile(string path);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern ImageMetadata LoadMetadataFromMemory(IntPtr dataPointer, int dataLength);
+
+            [DllImport("DirectXTexLib_x86",CharSet=CharSet.Unicode)]
+            public static extern long LoadTextureFromFile(IntPtr device, string path,out IntPtr resource, int miplevels);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern long SaveTextureToFile(IntPtr device,IntPtr context,IntPtr resource, string path,int format);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern long LoadTextureFromMemory(IntPtr device, IntPtr dataPointer, int dataLength, out IntPtr resource);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern long SaveCompressedTextureToFile(IntPtr device, IntPtr context, IntPtr resource, string path, int blocktype);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern long SaveCompressedTextureToMemory(IntPtr device, IntPtr context, IntPtr resource, int blocktype, out IntPtr data, out int size, out IntPtr blobPtr);
+
+            [DllImport("DirectXTexLib_x86", CharSet = CharSet.Unicode)]
+            public static extern void DeleteBlob(IntPtr blob);
+        }
+
+        public static void SaveToFileCompressed(DX11RenderContext device, DX11Texture2D texture, string path, DdsBlockType blockType)
+        {
+            long retcode = NativeMethods.SaveCompressedTextureToFile(device.Device.ComPointer, device.CurrentDeviceContext.ComPointer,
+                texture.Resource.ComPointer, path, (int)blockType);
+
+            if (retcode < 0)
+            {
+                throw new Exception("Failed to Save Texture");
+            }
+        }
+
+        public static void 
+            SaveToMemoryCompressed(DX11RenderContext device, DX11Texture2D texture, DdsBlockType blockType, out IntPtr data, out int size, out IntPtr blob)
+        {
+            long retcode = NativeMethods.SaveCompressedTextureToMemory(device.Device.ComPointer, device.CurrentDeviceContext.ComPointer,
+                texture.Resource.ComPointer, (int)blockType, out data, out size,out blob);
+
+            if (retcode < 0)
+            {
+                throw new Exception("Failed to Save Texture");
+            }
+        }
+    }
+}
