1ee89a8eba081b8d222149da40e73408783305d5
Julien Vulliet <julien@mrvux.com>
2015-12-29 18:00:53 +0300

    Add extension method for layer pin
    Factories now also builds

---
+++ Core/VVVV.DX11.Core/Utils/LayerPinExtentionsMethods.cs
@@ -0,0 +1,21 @@
+﻿using FeralTic.DX11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.DX11
+{
+    public static class LayerPinExtentionsMethods
+    {
+        public static void RenderParents(this Pin<DX11Resource<DX11Layer>> pin, DX11RenderContext context, DX11RenderSettings settings)
+        {
+            for (int i = 0; i < pin.SliceCount; i++)
+            {
+                pin[i][context].Render(context, settings);
+            }
+        }
+    }
+}
