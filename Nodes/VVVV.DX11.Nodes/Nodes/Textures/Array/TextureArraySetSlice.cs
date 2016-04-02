780533502178c6c1b24b58fd2b19bd5c8c4e7980
Julien Vulliet <julien@mrvux.com>
2016-04-02 17:40:54 +0300

    [Nodes] Fix mip level and slice build for SetSlice Texture array

--- Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/TextureArraySetSlice.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/TextureArraySetSlice.cs
@@ -38,7 +38,7 @@ namespace VVVV.DX11.Nodes
         {
             format = format == SlimDX.DXGI.Format.Unknown ? texture.Format : format;
             this.rtarr.Dispose();
-            this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, format);
+            this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, format, false, 1);
         }
 
         public void Apply(DX11Texture2D texture, int w, int h, int d, SlimDX.DXGI.Format format, int slice)
@@ -56,7 +56,7 @@ namespace VVVV.DX11.Nodes
 
             if (this.rtarr == null)
             {
-                this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, format, true, 1);
+                this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, format, false, 1);
             }
 
             this.shader.SelectTechnique("Render");
