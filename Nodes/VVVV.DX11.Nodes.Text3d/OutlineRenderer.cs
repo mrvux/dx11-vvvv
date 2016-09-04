c632e9fe94a0a25ec0d7bd447ba60e2e39979d6c
Julien Vulliet <julien@mrvux.com>
2016-09-04 17:49:32 +0200

    Add 3d text node

---
+++ Nodes/VVVV.DX11.Nodes.Text3d/OutlineRenderer.cs
@@ -0,0 +1,156 @@
+﻿using SharpDX.Direct2D1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DWriteFactory = SharpDX.DirectWrite.Factory;
+using D2DFactory = SharpDX.Direct2D1.Factory;
+using D2DGeometry = SharpDX.Direct2D1.Geometry;
+using SharpDX.DirectWrite;
+using SharpDX;
+
+
+
+namespace VVVV.DX11.Text3d
+{
+    public class OutlineRenderer : SharpDX.DirectWrite.TextRendererBase
+    {
+        private readonly D2DFactory factory;
+        private SharpDX.Direct2D1.Geometry geometry = null;
+
+        public OutlineRenderer(D2DFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public override SharpDX.Result DrawGlyphRun(object clientDrawingContext, float baselineOriginX, float baselineOriginY, MeasuringMode measuringMode, GlyphRun glyphRun, GlyphRunDescription glyphRunDescription, SharpDX.ComObject clientDrawingEffect)
+        {
+            Color4 c = Color4.White;
+            if (clientDrawingEffect != null)
+            {
+                if (clientDrawingEffect is SharpDX.Direct2D1.SolidColorBrush)
+                {
+                    var sb = (SharpDX.Direct2D1.SolidColorBrush)clientDrawingEffect;
+                    c = sb.Color;
+                }
+            }
+
+            if (glyphRun.Indices.Length > 0)
+            {
+                PathGeometry pg = new PathGeometry(this.factory);
+
+                GeometrySink sink = pg.Open();
+
+                glyphRun.FontFace.GetGlyphRunOutline(glyphRun.FontSize, glyphRun.Indices, glyphRun.Advances, glyphRun.Offsets, glyphRun.IsSideways, glyphRun.BidiLevel % 2 == 1, sink as SimplifiedGeometrySink);
+                sink.Close();
+
+                TransformedGeometry tg = new TransformedGeometry(this.factory, pg, Matrix3x2.Translation(baselineOriginX, baselineOriginY) * Matrix3x2.Scaling(1.0f, -1.0f));
+
+                pg.Dispose();
+
+                //Transform from baseline
+
+                this.AddGeometry(tg);
+
+                return SharpDX.Result.Ok;
+            }
+            else
+            {
+                return SharpDX.Result.Ok;
+            }
+
+        }
+
+        public override Result DrawUnderline(object clientDrawingContext, float baselineOriginX, float baselineOriginY, ref Underline underline, ComObject clientDrawingEffect)
+        {
+            PathGeometry pg = new PathGeometry(this.factory);
+            GeometrySink sink = pg.Open();
+
+            Vector2 topLeft = new Vector2(0.0f, underline.Offset);
+            sink.BeginFigure(topLeft, FigureBegin.Filled);
+            topLeft.X += underline.Width;
+            sink.AddLine(topLeft);
+            topLeft.Y += underline.Thickness;
+            sink.AddLine(topLeft);
+            topLeft.X -= underline.Width;
+            sink.AddLine(topLeft);
+            sink.EndFigure(FigureEnd.Closed);
+            sink.Close();
+
+
+            TransformedGeometry tg = new TransformedGeometry(this.factory, pg, Matrix3x2.Translation(baselineOriginX, baselineOriginY) * Matrix3x2.Scaling(1.0f, -1.0f));
+            pg.Dispose();
+
+            this.AddGeometry(tg);
+            return Result.Ok;
+        }
+
+        public override Result DrawStrikethrough(object clientDrawingContext, float baselineOriginX, float baselineOriginY, ref Strikethrough strikethrough, ComObject clientDrawingEffect)
+        {
+            PathGeometry pg = new PathGeometry(this.factory);
+            GeometrySink sink = pg.Open();
+
+            Vector2 topLeft = new Vector2(0.0f, strikethrough.Offset);
+            sink.BeginFigure(topLeft, FigureBegin.Filled);
+            topLeft.X += strikethrough.Width;
+            sink.AddLine(topLeft);
+            topLeft.Y += strikethrough.Thickness;
+            sink.AddLine(topLeft);
+            topLeft.X -= strikethrough.Width;
+            sink.AddLine(topLeft);
+            sink.EndFigure(FigureEnd.Closed);
+            sink.Close();
+
+
+            TransformedGeometry tg = new TransformedGeometry(this.factory, pg, Matrix3x2.Translation(baselineOriginX, baselineOriginY) * Matrix3x2.Scaling(1.0f, -1.0f));
+            pg.Dispose();
+
+            this.AddGeometry(tg);
+            return Result.Ok;
+        }
+
+
+        public override SharpDX.Mathematics.Interop.RawMatrix3x2 GetCurrentTransform(object clientDrawingContext)
+        {
+            return SharpDX.Matrix3x2.Identity;
+        }
+
+        public override bool IsPixelSnappingDisabled(object clientDrawingContext)
+        {
+            return true;
+        }
+
+        public override float GetPixelsPerDip(object clientDrawingContext)
+        {
+            return 1.0f;
+        }
+
+        public SharpDX.Direct2D1.Geometry GetGeometry()
+        {
+            return this.geometry;
+        }
+
+        protected void AddGeometry(D2DGeometry geom)
+        {
+            if (this.geometry == null)
+            {
+                this.geometry = geom;
+            }
+            else
+            {
+                PathGeometry pg = new PathGeometry(this.factory);
+
+                GeometrySink sink = pg.Open();
+
+                this.geometry.Combine(geom, CombineMode.Union, sink);
+
+                sink.Close();
+
+                this.geometry = pg;
+            }
+        }
+    }
+
+}
---
+++ Nodes/VVVV.DX11.Nodes.Text3d/Text3dNode.cs
@@ -0,0 +1,135 @@
+﻿using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+using SharpDX.DirectWrite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.PluginInterfaces.V2;
+using DWriteFactory = SharpDX.DirectWrite.Factory;
+using D2DFactory = SharpDX.Direct2D1.Factory;
+using D2DGeometry = SharpDX.Direct2D1.Geometry;
+
+
+using SharpDX.Direct2D1;
+using FeralTic.DX11.Geometry;
+using SharpDX;
+using System.Runtime.InteropServices;
+
+using InputElement = SlimDX.Direct3D11.InputElement;
+using VVVV.DX11.Text3d;
+
+namespace VVVV.DX11.Nodes
+{
+    public class PointSnapper
+    {
+        public static Vector2 SnapPoint(Vector2 input)
+        {
+            return input;
+        }
+    }
+
+    [PluginInfo(Name = "Text", Category = "DX11.Geometry", Version = "", Author = "vux", Help ="Builds 3d text geometry", Warnings ="Please note that there are no UV for the text")]
+    public unsafe class TextMeshNode : DX11BaseVertexPrimitiveNode
+    {
+        [Input("Text", DefaultString = "DX11")]
+        protected IDiffSpread<string> FText;
+
+        [Input("Font", EnumName = "DirectWrite_Font_Families")]
+        protected IDiffSpread<EnumEntry> FFontInput;
+
+        [Input("Font Size", DefaultValue = 32)]
+        protected IDiffSpread<int> FFontSize;
+
+        [Input("Horizontal Aligment")]
+        protected IDiffSpread<TextAlignment> FHAlignment;
+
+        [Input("Vertical Aligment")]
+        protected IDiffSpread<ParagraphAlignment> FVAlignment;
+
+        [Input("Extrude Amount", DefaultValue = 1.0)]
+        protected IDiffSpread<float> FExtrude;
+
+        private static SharpDX.Direct2D1.Factory d2dFactory;
+        private static SharpDX.DirectWrite.Factory dwFactory;
+
+        protected override DX11VertexGeometry GetGeom(DX11RenderContext device, int slice)
+        {
+            if (d2dFactory == null)
+            {
+                d2dFactory = new D2DFactory();
+                dwFactory = new DWriteFactory(SharpDX.DirectWrite.FactoryType.Shared);
+            }
+
+            TextFormat fmt = new TextFormat(dwFactory, this.FFontInput[slice].Name, FFontSize[slice]);
+
+            TextLayout tl = new TextLayout(dwFactory, FText[slice], fmt, 0.0f, 32.0f);
+            tl.WordWrapping = WordWrapping.NoWrap;
+            tl.TextAlignment = FHAlignment[slice];
+            tl.ParagraphAlignment = FVAlignment[slice];
+
+            OutlineRenderer renderer = new OutlineRenderer(d2dFactory);
+            Extruder ex = new Extruder(d2dFactory);
+
+            tl.Draw(renderer, 0.0f, 0.0f);
+
+            var result = ex.GetVertices(renderer.GetGeometry(), this.FExtrude[slice]);
+
+            Vector3 min = new Vector3(float.MaxValue);
+            Vector3 max = new Vector3(float.MinValue);
+
+            result.ForEach(pn =>
+            {
+                min.X = pn.Position.X < min.X ? pn.Position.X : min.X;
+                min.Y = pn.Position.Y < min.Y ? pn.Position.Y : min.Y;
+                min.Z = pn.Position.Z < min.Z ? pn.Position.Z : min.Z;
+
+                max.X = pn.Position.X > max.X ? pn.Position.X : max.X;
+                max.Y = pn.Position.Y > max.Y ? pn.Position.Y : max.Y;
+                max.Z = pn.Position.Z > max.Z ? pn.Position.Z : max.Z;
+            });
+
+            SlimDX.DataStream ds = new SlimDX.DataStream(result.Count * Pos3Norm3VertexSDX.VertexSize, true, true);
+            ds.Position = 0;
+
+            ds.WriteRange(result.ToArray());
+
+            ds.Position = 0;
+
+            var vbuffer = new SlimDX.Direct3D11.Buffer(device.Device, ds, new SlimDX.Direct3D11.BufferDescription()
+            {
+                BindFlags = SlimDX.Direct3D11.BindFlags.VertexBuffer,
+                CpuAccessFlags = SlimDX.Direct3D11.CpuAccessFlags.None,
+                OptionFlags = SlimDX.Direct3D11.ResourceOptionFlags.None,
+                SizeInBytes = (int)ds.Length,
+                Usage = SlimDX.Direct3D11.ResourceUsage.Default
+            });
+
+            ds.Dispose();
+
+            DX11VertexGeometry vg = new DX11VertexGeometry(device);
+            vg.InputLayout = Pos3Norm3VertexSDX.Layout;
+            vg.Topology = SlimDX.Direct3D11.PrimitiveTopology.TriangleList;
+            vg.VertexBuffer = vbuffer;
+            vg.VertexSize = Pos3Norm3VertexSDX.VertexSize;
+            vg.VerticesCount = result.Count;
+            vg.HasBoundingBox = true;
+            vg.BoundingBox = new SlimDX.BoundingBox(new SlimDX.Vector3(min.X, min.Y, min.Z), new SlimDX.Vector3(max.X, max.Y, max.Z));
+
+            return vg;
+        }
+
+        protected override bool Invalidate()
+        {
+            bool b = false;
+
+            b = b || this.FText.IsChanged || this.FExtrude.IsChanged || this.FFontSize.IsChanged
+                || this.FFontInput.IsChanged || this.FHAlignment.IsChanged || this.FVAlignment.IsChanged;
+
+            return b;
+
+        }
+    }
+}
