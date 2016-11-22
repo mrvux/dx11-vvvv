060b8c52b56e2f2ae771a4f6dbbd2ba023b405dd
julien vulliet <julien@mrvux.com>
2016-11-22 14:49:32 +0100

    Add anisotropic sampler node

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/RenderStates/SamplerStateAnisotropicNode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using SlimDX.Direct3D11;
+using VVVV.Utils.VColor;
+using SlimDX;
+using VVVV.PluginInterfaces.V1;
+using System.ComponentModel.Composition;
+using FeralTic.DX11;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "Anisotropicr", Category = "DX11", Version = "Sampler", Author = "vux")]
+    public class DX11SamplerStateAnisotropicNode : IPluginEvaluate
+    {
+        [Input("Address Mode", DefaultEnumEntry = "Wrap")]
+        protected IDiffSpread<TextureAddressMode> FInAddress;
+
+        [Input("Maximum Anisotropy", DefaultValue = 1)]
+        protected IDiffSpread<int> FInMaximumAnisotropy;
+
+        [Input("Border Color", DefaultColor = new double[] { 0, 0, 0, 1 })]
+        protected IDiffSpread<Color4> FInBorderColor;
+
+        [Output("Sampler")]
+        protected ISpread<SamplerDescription> FOutSampler;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FInAddress.IsChanged || this.FInMaximumAnisotropy.IsChanged
+                || this.FInBorderColor.IsChanged)
+            {
+                this.FOutSampler.SliceCount = SpreadMax;
+
+                for (int i = 0; i < SpreadMax; i++)
+                {
+                    this.FOutSampler[i] = new SamplerDescription()
+                    {
+                        AddressU = this.FInAddress[i],
+                        AddressV = this.FInAddress[i],
+                        AddressW = this.FInAddress[i],
+                        BorderColor = this.FInBorderColor[i],
+                        ComparisonFunction =  Comparison.Always,
+                        Filter = Filter.Anisotropic,
+                        MaximumAnisotropy = this.FInMaximumAnisotropy[i],
+                        MaximumLod = float.MaxValue,
+                        MinimumLod = float.MinValue,
+                        MipLodBias = 0
+                    };
+                }
+            }
+        }
+    }
+}
+
+
