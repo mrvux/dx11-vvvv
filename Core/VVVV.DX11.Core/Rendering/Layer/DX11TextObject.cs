f32db205d4541673946c07a6d542e8f4565de194
Julien Vulliet <julien@mrvux.com>
2016-12-06 21:29:33 +0300

    [Core] Add TextObject and Text cache

---
+++ Core/VVVV.DX11.Core/Rendering/Layer/DX11TextObject.cs
@@ -0,0 +1,55 @@
+﻿using SlimDX;
+using SlimDX.DirectWrite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVV.DX11
+{
+    public class TextObject
+    {
+        public TextFormat TextFormat;
+        public string Text;
+        public Matrix Matrix;
+        public Color4 Color;
+    }
+
+    public class DX11CachedText
+    {
+        public DX11CachedText(TextLayout tl, Matrix mat, Color4 color)
+        {
+            this.TextLayout = tl;
+            this.Matrix = mat;
+            this.Color = color;
+        }
+
+        public TextLayout TextLayout;
+        public Matrix Matrix;
+        public Color4 Color;
+    }
+
+    public class DX11TextObjectCache : IDisposable
+    {
+        public DX11CachedText[] objects;
+
+        public DX11TextObjectCache(IEnumerable<DX11CachedText> objects)
+        {
+            this.objects = objects.ToArray();
+        }
+
+        public DX11CachedText Get(int index)
+        {
+            return objects[index];
+        }
+
+        public void Dispose()
+        {
+            for (int i = 0; i < this.objects.Length; i++)
+            {
+                this.objects[i].TextLayout.Dispose();
+            }
+        }
+    }
+}
