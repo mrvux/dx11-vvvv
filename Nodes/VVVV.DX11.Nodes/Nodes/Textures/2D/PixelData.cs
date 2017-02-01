c6c0affdc9b764ef7d344560eb3d842aa55d67c3
Julien Vulliet <julien@mrvux.com>
2017-02-01 16:41:30 +0100

    [Nodes] PixelData preserves stream if possible, and has Apply Stride pin, to perform slow copy but packing pixels.

--- Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/PixelData.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/PixelData.cs
@@ -16,14 +16,24 @@ using FeralTic.DX11.Resources;
 
 using System.IO;
 using SlimDX;
+using System.Runtime.InteropServices;
+using FeralTic.DX11.Utils;
 
 namespace VVVV.DX11.Nodes
 {
     [PluginInfo(Name = "PixelData", Category = "DX11.Texture", Version = "2d", Author = "vux", Credits = "vux")]
-    public class TexturePixelDataNode : IPluginEvaluate, IDX11ResourceDataRetriever, IPartImportsSatisfiedNotification
+    public unsafe class TexturePixelDataNode : IPluginEvaluate, IDX11ResourceDataRetriever, IPartImportsSatisfiedNotification
     {
+        private FormatHelper formatHelper = FormatHelper.Instance;
+
+        [DllImport("msvcrt.dll", SetLastError = false)]
+        static extern IntPtr memcpy(byte* dest, byte* src, int count);
+
         [Input("Texture In", IsSingle =true, AutoValidate =false)]
         protected Pin<DX11Resource<DX11Texture2D>> FTextureIn;
+
+        [Input("Apply Stride")]
+        protected ISpread<bool> FApplyStride;
 
         [Input("Read", IsBang = true)]
         protected ISpread<bool> FRead;
@@ -55,7 +65,8 @@ namespace VVVV.DX11.Nodes
             FStreamOut.SliceCount = 0;
         }
 
-        Stream lastStream;
+        DataStream lastStream;
+        byte[] binter = new byte[0];
 
         #region IPluginEvaluate Members
 
@@ -79,11 +90,7 @@ namespace VVVV.DX11.Nodes
 
                     if (this.FTextureIn[0].Contains(this.AssignedContext))
                     {
-                        if (this.lastStream != null)
-                        {
-                            this.lastStream.Dispose();
-                            this.lastStream = null;
-                        }
+                        
 
                         var texture = this.FTextureIn[0][context];
                         var staging = new DX11StagingTexture2D(context, texture.Width, texture.Height, texture.Format);
@@ -93,16 +100,53 @@ namespace VVVV.DX11.Nodes
 
                         strideOut[0] = db.RowPitch;
 
-                        MemoryStream ms = new MemoryStream((int)db.Data.Length);
-                        db.Data.CopyTo(ms);
-                        ms.Position = 0;
+                        if (this.lastStream != null)
+                        {
+                            if (this.lastStream.Length != db.Data.Length)
+                            {
+                                this.lastStream.Dispose();
+                                this.lastStream = null;
+                            }
+                        }
+
+                        if (this.lastStream == null)
+                        {
+                            this.lastStream = new DataStream((int)db.Data.Length, true, true);
+                        }
+
+                        this.lastStream.Position = 0;
+
+                        int pixelStride = formatHelper.GetSize(texture.Format);
+                        int dstStride = pixelStride * texture.Width;
+
+                        if (FApplyStride[0])
+                        {
+                            if (this.binter.Length != db.RowPitch)
+                            {
+                                this.binter = new byte[db.RowPitch];
+                            }
+
+                            byte* destPointer = (byte*)this.lastStream.DataPointer.ToPointer();
+                            byte* srcPointer = (byte*)db.Data.DataPointer.ToPointer();
+
+                            for (int i = 0; i < texture.Height; i++)
+                            {
+                                memcpy(destPointer, srcPointer, dstStride);
+                                destPointer += dstStride;
+                                srcPointer += db.RowPitch;
+                            }
+                        }
+                        else
+                        {
+                            db.Data.CopyTo(this.lastStream);
+                        }
+                        this.lastStream.Position = 0;
 
                         staging.UnLock();
                         staging.Dispose();
 
-                        FStreamOut[0] = ms;
+                        FStreamOut[0] = this.lastStream;
                         FStreamOut.Flush(true);
-                        this.lastStream = ms;
                     }
                 }
             }
