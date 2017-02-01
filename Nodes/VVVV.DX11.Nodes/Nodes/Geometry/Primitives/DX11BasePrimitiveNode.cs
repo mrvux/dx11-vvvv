a05f851b23cb3414cada19620f617b217979f804
Julien Vulliet <julien@mrvux.com>
2017-02-01 16:38:07 +0100

    [Nodes] Base geometry prevents leak if it is contained in a non evaluate pin.
    [Nodes] All basic primitives using base geometry now have "Keep In memory" Pin, to avoid destruction

--- Nodes/VVVV.DX11.Nodes/Nodes/Geometry/Primitives/DX11BasePrimitiveNode.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Geometry/Primitives/DX11BasePrimitiveNode.cs
@@ -15,6 +15,8 @@ namespace VVVV.DX11.Nodes
 {
     public abstract class DX11BasePrimitiveNode : IPluginEvaluate, IDX11ResourceHost, IDisposable
     {
+        [Input("Keep In Memory", IsSingle =true, Order = 5000, Visibility =PinVisibility.OnlyInspector)]
+        protected ISpread<bool> FKeepInMemory;
 
         [Output("Geometry Out", Order = 5)]
         protected Pin<DX11Resource<DX11IndexedGeometry>> FOutput;
@@ -55,12 +57,16 @@ namespace VVVV.DX11.Nodes
                     DX11IndexedGeometry geom = this.GetGeom(context, i);
                     this.FOutput[i][context] = geom;   
                 }
+                this.FInvalidate = false;
             }
         }
 
         public void Destroy(DX11RenderContext context, bool force)
         {
-            this.FOutput.SafeDisposeAll(context);
+            if (FKeepInMemory[0] == false || force)
+            {
+                this.FOutput.SafeDisposeAll(context);
+            }
         }
 
         public void Dispose()
