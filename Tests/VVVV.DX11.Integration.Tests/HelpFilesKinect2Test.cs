c3c5a2a460b39b82b852c5039063f964b3082995
Julien Vulliet <julien@mrvux.com>
2017-02-13 18:03:43 +0100

    [Build+Tests] Clean up

---
+++ Tests/VVVV.DX11.Integration.Tests/HelpFilesKinect2Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VVVV.DX11.Nodes;
+using VVVV.PluginInterfaces.V2;
+using System.Linq;
+using System.IO;
+using VVVV.DX11.Nodes.Nodes.Text;
+using VVVV.DX11.Nodes.AssetImport;
+using MSKinect.Nodes;
+
+namespace VVVV.DX11.Integration.Tests
+{
+    [TestClass]
+    public partial class HelpFilesTests
+    {
+        [TestMethod]
+        public void VVVVDX11Kinect2NodesHelpPatchesTest()
+        {
+            Assembly assembly = Assembly.GetAssembly(typeof(VVVV.DX11.Nodes.MSKinect.KinectRayTextureNode));
+
+            int missingCount = 0;
+            StringBuilder sb = new StringBuilder();
+
+            string path = System.IO.Path.GetDirectoryName(assembly.Location);
+            path = Path.Combine(path, helpFilesRelativePath, "kinect2");
+
+            foreach (Type t in assembly.GetExportedTypes())
+            {
+                if (typeof(IPluginEvaluate).IsAssignableFrom(t) && !t.IsAbstract)
+                {
+                    var attr = t.GetCustomAttributes<PluginInfoAttribute>().FirstOrDefault();
+                    if (attr != null)
+                    {
+                        string nodeName = attr.Systemname;
+                        string helpFile = nodeName + " help.v4p";
+
+                        if (!File.Exists(Path.Combine(path, helpFile)))
+                        {
+                            sb.AppendLine(nodeName);
+                            missingCount++;
+                        }
+                    }
+                }
+            }
+
+            string msg = sb.ToString();
+            Assert.AreEqual(missingCount, 0, "Missing help files: \r\n" + sb.ToString());
+        }
+    }
+}
--- Tests/VVVV.DX11.Integration.Tests/HelpFilesTest.cs
+++ Tests/VVVV.DX11.Integration.Tests/HelpFilesTest.cs
@@ -188,42 +188,6 @@ namespace VVVV.DX11.Integration.Tests
             Assert.AreEqual(missingCount, 0, "Missing help files: \r\n" + sb.ToString());
         }
 
-
-        [TestMethod]
-        public void VVVVDX11Kinect2NodesHelpPatchesTest()
-        {
-            Assembly assembly = Assembly.GetAssembly(typeof(VVVV.DX11.Nodes.MSKinect.KinectRayTextureNode));
-
-            int missingCount = 0;
-            StringBuilder sb = new StringBuilder();
-
-            string path = System.IO.Path.GetDirectoryName(assembly.Location);
-            path = Path.Combine(path, helpFilesRelativePath, "kinect2");
-
-            foreach (Type t in assembly.GetExportedTypes())
-            {
-                if (typeof(IPluginEvaluate).IsAssignableFrom(t) && !t.IsAbstract)
-                {
-                    var attr = t.GetCustomAttributes<PluginInfoAttribute>().FirstOrDefault();
-                    if (attr != null)
-                    {
-                        string nodeName = attr.Systemname;
-                        string helpFile = nodeName + " help.v4p";
-
-                        if (!File.Exists(Path.Combine(path, helpFile)))
-                        {
-                            sb.AppendLine(nodeName);
-                            missingCount++;
-                        }
-                    }
-                }
-            }
-
-            string msg = sb.ToString();
-            Assert.AreEqual(missingCount, 0, "Missing help files: \r\n" + sb.ToString());
-        }
-
-
         [TestMethod]
         public void VVVVDX11QRCodeNodesHelpPatchesTest()
         {
