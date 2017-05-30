57df78fdb92c86733266c44332bbdf7b51432243
Julien Vulliet <julien@mrvux.com>
2017-05-30 19:55:11 +0300

    Add constraint collections and node to retrieve those

---
+++ Nodes/VVVV.DX11.Nodes.Bullet/Core/IConstraintCollection.cs
@@ -0,0 +1,20 @@
+﻿using BulletSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVV.Bullet.Core
+{
+    /// <summary>
+    /// Interface for an element that contains constraints
+    /// </summary>
+    public interface IConstraintCollection
+    {
+        /// <summary>
+        /// List of constraints
+        /// </summary>
+        List<TypedConstraint> Constraints { get; }
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes.Bullet/Nodes/World/Retrieve/BulletGetConstraintsNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SlimDX;
+
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VMath;
+
+using BulletSharp;
+using BulletSharp.SoftBody;
+using VVVV.DataTypes.Bullet;
+using VVVV.Bullet.DataTypes;
+using VVVV.Bullet.Core;
+
+namespace VVVV.Nodes.Bullet
+{
+    [PluginInfo(Name = "Constraints", Category = "Bullet", Version ="", Author = "vux")]
+    public class BulletGetConstraintsNode : IPluginEvaluate
+    {
+        [Input("World", IsSingle = true)]
+        protected ISpread<IConstraintCollection> FWorld;
+
+        [Output("Constraints")]
+        protected ISpread<TypedConstraint> FConstraints;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FWorld[0] != null)
+            {
+                var constraints = this.FWorld[0].Constraints;
+                this.FConstraints.SliceCount = constraints.Count;
+
+                var outputBuffer = this.FConstraints.Stream.Buffer;
+                for (int i = 0; i < constraints.Count; i++)
+                {
+                    outputBuffer[i] = constraints[i];
+                }
+                this.FConstraints.Flush(true);
+            }
+            else
+            {
+                this.FConstraints.SliceCount = 0;
+            }
+        }
+    }
+}
