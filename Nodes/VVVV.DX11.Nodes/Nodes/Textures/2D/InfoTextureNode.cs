f62b1c1271776263c77ba3897a2abb26d49ebc89
Julien Vulliet <julien@mrvux.com>
2017-06-01 02:46:09 +0300

    [Nodes] Add creation time on info (debug timestamp)

--- Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/InfoTextureNode.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/2D/InfoTextureNode.cs
@@ -44,6 +44,9 @@ namespace VVVV.DX11.Nodes
         [Output("Resource Pointer", Visibility=PinVisibility.OnlyInspector)]
         protected ISpread<int> FOutPointer;
 
+        [Output("Creation Time", Visibility = PinVisibility.OnlyInspector)]
+        protected ISpread<int> FOutCreationTime;
+
         [Import()]
         protected IPluginHost FHost;
 
@@ -90,6 +93,7 @@ namespace VVVV.DX11.Nodes
                             this.FOutAAQuality[i] = tdesc.SampleDescription.Quality;
                             this.FOutArraySize[i] = tdesc.ArraySize;
                             this.FOutPointer[i] = this.FTextureIn[i][this.AssignedContext].Resource.ComPointer.ToInt32();
+                            this.FOutCreationTime[i] = this.FTextureIn[i][this.AssignedContext].Resource.CreationTime;
                         }
                         else
                         {
@@ -120,6 +124,7 @@ namespace VVVV.DX11.Nodes
             this.FOutAAQuality.SliceCount = 0;
             this.FOutArraySize.SliceCount = 0;
             this.FOutPointer.SliceCount = 0;
+            this.FOutCreationTime.SliceCount = 0;
         }
 
         private void SetDefault(int i)
@@ -132,6 +137,7 @@ namespace VVVV.DX11.Nodes
             this.FOutAAQuality[i] = -1;
             this.FOutArraySize[i] = -1;
             this.FOutPointer[i] = -1;
+            this.FOutCreationTime[i] = 0;
         }
 
 
