fb82a7c143f583fb2cd33a7515aca3f99aa2ba73
Julien Vulliet <julien@mrvux.com>
2018-02-06 03:32:48 +0300

    Add WithinSphereValidator

---
+++ Core/VVVV.DX11.Lib/Rendering/Validators/DX11WithinSphereValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FeralTic.Core.Maths;
+using SlimDX;
+
+namespace VVVV.DX11.Validators
+{
+    public class WithinSphereValidator : IDX11ObjectValidator
+    {
+        public bool Enabled { get; set; }
+        public BoundingSphere BoundingSphere;
+
+        public void SetGlobalSettings(DX11RenderSettings settings)
+        {
+
+        }
+
+        public bool Validate(DX11ObjectRenderSettings obj)
+        {
+            if (obj.Geometry.HasBoundingBox == false)
+                return true;
+
+            Matrix worldMatrix = obj.WorldTransform;
+            BoundingBox boundingBox = obj.Geometry.BoundingBox;
+            boundingBox.Maximum = Vector3.TransformCoordinate(boundingBox.Maximum, worldMatrix);
+            boundingBox.Minimum = Vector3.TransformCoordinate(boundingBox.Minimum, worldMatrix);
+            return BoundingSphere.Contains(this.BoundingSphere, boundingBox) != ContainmentType.Disjoint;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Validators/WithinSphereValidatorNode.cs
@@ -0,0 +1,35 @@
+﻿using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.DX11.Validators;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "WithinSphere", Category = "DX11.Validator", Help = "Validates objects and only draw the ones that are within a bounding sphere", Author ="vux")]
+    public class ValidatorWithinSphereNode : IPluginEvaluate
+    {
+        [Input("Center", DefaultValue = 0.0)]
+        public ISpread<Vector3> FInput;
+
+        [Input("Radius", DefaultValue = 0.5)]
+        public ISpread<float> FRadius;
+
+        [Input("Enabled", DefaultValue = 1)]
+        protected ISpread<bool> FInEnabled;
+
+        [Output("Output")]
+        public ISpread<WithinSphereValidator> FOutput;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FOutput[0] == null) { this.FOutput[0] = new WithinSphereValidator(); }
+            this.FOutput[0].Enabled = this.FInEnabled[0];
+            this.FOutput[0].BoundingSphere = new BoundingSphere(this.FInput[0], this.FRadius[0]);
+            this.FOutput[0].Reset();
+        }
+    }
+}
