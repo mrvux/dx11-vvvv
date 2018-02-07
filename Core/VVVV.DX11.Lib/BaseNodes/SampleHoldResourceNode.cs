8d721bdaac06f1c939b4e052257a07493cd3f82e
Julien Vulliet <julien@mrvux.com>
2018-02-07 18:13:03 +0300

    [Nodes] Add S+H for texture2d/3d and structured buffers

---
+++ Core/VVVV.DX11.Lib/BaseNodes/SampleHoldResourceNode.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+using VVVV.PluginInterfaces.V2;
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+namespace VVVV.DX11.Nodes
+{
+    public abstract class SampleHoldResourceNode<T> : IPluginEvaluate, IDX11ResourceHost, IDX11UpdateBlocker, IDisposable where T : IDX11Resource
+    {
+       [Input("Input", AutoValidate =false)]
+        protected Pin<DX11Resource<T>> input;
+
+        [Input("Set", IsBang =true, DefaultValue =1, IsSingle =true)]
+        protected ISpread<bool> apply;
+
+        [Input("Set On Create", DefaultValue = 1, IsSingle = true, Visibility =PinVisibility.OnlyInspector)]
+        protected ISpread<bool> setoncreate;
+
+        [Input("Flush", DefaultValue = 0, IsSingle = true, Visibility = PinVisibility.OnlyInspector)]
+        protected ISpread<bool> flush;
+
+        [Output("Output")]
+        protected ISpread<DX11Resource<T>> output;
+
+        private bool first = true;
+
+        private bool needCopy;
+
+        private T currentResource;
+
+        public bool Enabled
+        {
+            get { return this.needCopy; }
+        }
+
+        public void Evaluate(int SpreadMax)
+        {
+            this.needCopy = false;
+
+            if (this.apply[0] || (this.first && this.setoncreate[0]))
+            {
+                this.input.Sync();
+
+                //Destroy resource on nil
+                if (SpreadMax == 0 && this.output.SliceCount == 1)
+                {
+                    this.output.SafeDisposeAll();
+                }
+
+                this.output.SliceCount = SpreadMax == 0 ? 0 : 1;
+                this.output.CreateIfNull();
+
+                this.needCopy = this.output.SliceCount > 0; // need copy only if input is not nil
+
+            }
+
+            this.first = false;
+        }
+
+        public void Update(DX11RenderContext context)
+        {
+            //Note no need to check if copy needed, as blocker will auto handle that
+            if (this.input[0].Contains(context))
+            {
+                var inputResource = this.input[0][context];
+
+                if (inputResource != null)
+                {
+                    this.ProcessResource(context, this.input[0][context], ref this.currentResource);
+                    if (this.currentResource != null)
+                    {
+                        this.output[0][context] = this.currentResource;
+                    }
+                    else
+                    {
+                        this.output[0].Remove(context);
+                    }
+                }
+                else
+                {
+                    this.output.SafeDisposeAll(context);
+                }
+
+                if (this.flush[0])
+                {
+                    context.CurrentDeviceContext.Flush();
+                }
+            }
+        }
+
+        protected abstract void ProcessResource(DX11RenderContext context, T inputResource, ref T outputResource);
+
+        public void Destroy(DX11RenderContext context, bool force)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            this.output.SafeDisposeAll();
+        }
+
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Generics/SampleHoldNodes.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using VVVV.PluginInterfaces.V2;
+
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+
+using SlimDX.Direct3D11;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "S+H", Category = "DX11.Texture", Version = "2d", Author = "vux", Help = "Sample and Hold - if set is 1 just passes the input through(makes a copy of it), when set is 0 keeps the copy instead(and block upwards evaluation and rendering")]
+    public class SampleHoldTexture2DNode : SampleHoldResourceNode<DX11Texture2D>
+    {
+        private Texture2DDescription currentDescription;
+
+        protected override void ProcessResource(DX11RenderContext context, DX11Texture2D inputResource, ref DX11Texture2D outputResource)
+        {
+            if (outputResource != null)
+            {
+                if (currentDescription != outputResource.Description)
+                {
+                    outputResource.Dispose();
+                    outputResource = null;
+                }
+                
+            }
+
+            if (outputResource == null)
+            {
+                this.currentDescription = inputResource.Description;
+
+                //Only thing we need is to make default pool, as otherwise immutable texture copy would fail
+                Texture2DDescription desc = this.currentDescription;
+                desc.Usage = ResourceUsage.Default;
+
+                outputResource = DX11Texture2D.FromDescription(context, desc);
+            }
+            context.CurrentDeviceContext.CopyResource(inputResource.Resource, outputResource.Resource);
+        }
+    }
+
+    [PluginInfo(Name = "S+H", Category = "DX11.Buffer", Version = "Structured", Author = "vux", Help ="Sample and Hold - if set is 1 just passes the input through(makes a copy of it), when set is 0 keeps the copy instead(and block upwards evaluation and rendering")]
+    public class SampleHoldStructuredNode : SampleHoldResourceNode<IDX11ReadableStructureBuffer>
+    {
+        private BufferDescription currentDescription;
+
+        protected override void ProcessResource(DX11RenderContext context, IDX11ReadableStructureBuffer inputResource, ref IDX11ReadableStructureBuffer outputResource)
+        {
+            if (outputResource != null)
+            {
+                if (currentDescription != outputResource.Buffer.Description)
+                {
+                    outputResource.Dispose();
+                    outputResource = null;
+                }
+            }
+
+            if (outputResource == null)
+            {
+                this.currentDescription = inputResource.Buffer.Description;
+                //Only thing we need is to make default pool, as otherwise immutable texture copy would fail
+                BufferDescription desc = this.currentDescription;
+                desc.Usage = ResourceUsage.Default;
+
+                outputResource = new DX11CopyDestStructuredBuffer(context.Device, desc);
+            }
+            context.CurrentDeviceContext.CopyResource(inputResource.Buffer, outputResource.Buffer);
+        }
+    }
+
+    [PluginInfo(Name = "S+H", Category = "DX11.Texture", Version = "3d", Author = "vux", Help = "Sample and Hold - if set is 1 just passes the input through(makes a copy of it), when set is 0 keeps the copy instead(and block upwards evaluation and rendering")]
+    public class SampleHoldTexture3DNode : SampleHoldResourceNode<DX11Texture3D>
+    {
+        private Texture3DDescription currentDescription;
+
+        protected override void ProcessResource(DX11RenderContext context, DX11Texture3D inputResource, ref DX11Texture3D outputResource)
+        {
+            if (outputResource != null)
+            {
+                if (currentDescription != outputResource.Resource.Description)
+                {
+                    outputResource.Dispose();
+                    outputResource = null;
+                }
+
+            }
+
+            if (outputResource == null)
+            {
+                this.currentDescription = inputResource.Resource.Description;
+
+                //Only thing we need is to make default pool, as otherwise immutable texture copy would fail
+                Texture3DDescription desc = this.currentDescription;
+                desc.Usage = ResourceUsage.Default;
+
+                outputResource = DX11Texture3D.FromDescription(context, desc);
+            }
+            context.CurrentDeviceContext.CopyResource(inputResource.Resource, outputResource.Resource);
+        }
+    }
+
+
+
+}
