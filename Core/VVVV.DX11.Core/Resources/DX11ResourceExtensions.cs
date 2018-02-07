f06e006a8290b0dca24e95e18904bf40ead214d7
Julien Vulliet <julien@mrvux.com>
2018-02-07 18:11:53 +0300

    New extension method to create elements, and update submodule

--- Core/VVVV.DX11.Core/Resources/DX11ResourceExtensions.cs
+++ Core/VVVV.DX11.Core/Resources/DX11ResourceExtensions.cs
@@ -11,6 +11,17 @@ namespace VVVV.DX11
 {
     public static class DX11ResourceExtensions
     {
+        public static void CreateIfNull<T>(this ISpread<DX11Resource<T>> spread) where T : IDX11Resource
+        {
+            for (int i = 0; i < spread.SliceCount; i++)
+            {
+                if (spread[i] == null)
+                {
+                    spread[i] = new DX11Resource<T>();
+                }
+            }
+        }
+
         public static void DisposeSpread<T>(this ISpread<T> spread) where T : IDisposable
         {
             for (int i = 0; i < spread.SliceCount; i++)
