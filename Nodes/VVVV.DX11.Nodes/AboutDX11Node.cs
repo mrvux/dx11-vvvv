0f7f14c790c02bb2a7cd70625795c16dadfd03d1
Julien Vulliet <julien@mrvux.com>
2018-02-28 19:43:52 +0300

    Add about node

---
+++ Nodes/VVVV.DX11.Nodes/AboutDX11Node.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V2;
+
+
+using FeralTic.DX11;
+using FeralTic.DX11.Queries;
+
+using VVVV.DX11.Lib.Devices;
+using VVVV.DX11.Lib.RenderGraph;
+using SlimDX.Direct3D11;
+using FeralTic.Utils;
+
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "About", Category = "DX11",Version="", Author = "vux",Tags= "", AutoEvaluate=true, Help ="Gets information about DirectX11 package, including author, contributors and patreon/private supporters")]
+    public class DX11AboutNode : IPluginEvaluate, IPartImportsSatisfiedNotification
+    {
+        private readonly string[] contributorList = new string[]
+        {
+            "unc",
+            "cloneproduction",
+            "sebllll",
+            "joreg",
+            "dotprodukt",
+            "alg",
+            "azeno",
+            "gumilastik",
+            "fibo",
+            "id144",
+            "mhusinsky",
+            "tmp",
+            "mholub",
+            "gregsn"
+        };
+
+        private readonly string[] supporterList = new string[]
+        {
+            "Irwin Quemener - CloneProduction"
+        };
+
+        [Output("Version")]
+        protected ISpread<string> version;
+
+        [Output("Author")]
+        protected ISpread<string> author;
+
+        [Output("Contributors")]
+        protected ISpread<string> contributors;
+
+        [Output("Supporters")]
+        protected ISpread<string> supporters;
+
+        #region IPluginEvaluate Members
+        public void Evaluate(int SpreadMax)
+        {
+        }
+
+        public void OnImportsSatisfied()
+        {
+            this.version[0] = "1.2";
+            this.author[0] = "vux";
+
+            this.contributors.AssignFrom(this.contributorList);
+            this.supporters.AssignFrom(this.supporterList);
+        }
+        #endregion
+    }
+}
