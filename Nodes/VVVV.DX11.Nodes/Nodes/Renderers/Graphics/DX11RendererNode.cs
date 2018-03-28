3ee868ced01f47c8bfe6f525c4682296a3ce74b7
tebjan <tebjan@vvvv.org>
2018-03-28 05:44:45 +0200

    added conditional output pins for Mouse, Keyboard and Touch events, also kept backward compatibility for older versions via reflection

--- Nodes/VVVV.DX11.Nodes/Nodes/Renderers/Graphics/DX11RendererNode.cs
+++ Nodes/VVVV.DX11.Nodes/Nodes/Renderers/Graphics/DX11RendererNode.cs
@@ -30,6 +30,9 @@ using FeralTic.DX11;
 using FeralTic.DX11.Resources;
 using VVVV.DX11.Nodes.Renderers.Graphics.Touch;
 using VVVV.DX11.Windows;
+using System.Reflection;
+using FormsMouseEventArgs = System.Windows.Forms.MouseEventArgs;
+using System.Reactive.Linq;
 
 namespace VVVV.DX11.Nodes
 {
@@ -675,10 +678,343 @@ namespace VVVV.DX11.Nodes
 
         }
 
+        [Import]
+        protected IIOFactory FIOFactory;
+
         public void OnImportsSatisfied()
         {
             this.FOutCtrl[0] = this;
             this.FOutRef[0] = (INode)this.FHost;
+
+            //add mouse event output for newer vvvv versions
+            var mouseType = typeof(Mouse);
+
+            //older mouse type
+            var olderParams = new Type[] { typeof(IObservable<MouseNotification>), typeof(bool) };
+            var olderConstructor = mouseType.GetConstructor(olderParams);
+            if(olderConstructor != null)
+            {
+                var oa = new OutputAttribute("Mouse Events");
+                oa.IsSingle = true;
+                var mouseEventOut = FIOFactory.CreatePin<Mouse>(oa);
+                mouseEventOut[0] = InitMouseEvents(olderConstructor, true);
+
+                CreateKeyboardEventsOut();
+                CreateTouchEventsOut();
+            }
+            else //newer mouse type
+            {
+                var newerParams = new Type[] { typeof(IObservable<MouseNotification>), typeof(bool), typeof(bool) };
+                var newerConstructor = mouseType.GetConstructor(newerParams);
+
+                if (newerConstructor != null)
+                {
+                    var oa = new OutputAttribute("Mouse Events");
+                    oa.IsSingle = true;
+                    var mouseEventOut = FIOFactory.CreatePin<Mouse>(oa);
+                    mouseEventOut[0] = InitMouseEvents(newerConstructor);
+
+                    CreateKeyboardEventsOut();
+                    CreateTouchEventsOut();
+                }
+            }
+        }
+
+        private Mouse InitMouseEvents(ConstructorInfo constructor, bool isOlderMouse = false)
+        {
+            var mouseDowns = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseDown")
+                .Select(p => p.EventArgs.ToMouseDownNotification(this));
+            var mouseMoves = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseMove")
+                .Select(p => p.EventArgs.ToMouseMoveNotification(this));
+            var mouseUps = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseUp")
+                .Select(p => p.EventArgs.ToMouseUpNotification(this));
+            var mouseClicks = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseClick")
+                .Select(p => p.EventArgs.ToMouseClickNotification(this));
+            var mouseDoubleClicks = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseDoubleClick")
+                .Select(p => p.EventArgs.ToMouseDoubleClickNotification(this));
+            var mouseWheels = Observable.FromEventPattern<FormsMouseEventArgs>(this, "MouseWheel")
+                .Select(p => p.EventArgs.ToMouseWheelNotification(this));
+
+            var mouseStream = mouseDowns
+                        .Merge<MouseNotification>(mouseMoves)
+                        .Merge(mouseUps)
+                        .Merge(mouseClicks)
+                        .Merge(mouseDoubleClicks)
+                        .Merge(mouseWheels);
+
+            if (isOlderMouse)
+                return (Mouse)constructor.Invoke(new object[] { mouseStream, false });
+            else
+                return (Mouse)constructor.Invoke(new object[] { mouseStream, false, true });
+        }
+
+        private void CreateKeyboardEventsOut()
+        {
+            var oa = new OutputAttribute("Keyboard Events");
+            oa.IsSingle = true;
+            var keyboardEventOut = FIOFactory.CreatePin<Keyboard>(oa);
+
+            var keyDowns = Observable.FromEventPattern<KeyEventArgs>(this, "KeyDown")
+                .Select(p => p.EventArgs.ToKeyDownNotification());
+            var keyUps = Observable.FromEventPattern<KeyEventArgs>(this, "KeyUp")
+                .Select(p => p.EventArgs.ToKeyUpNotification());
+            var keyPresses = Observable.FromEventPattern<KeyPressEventArgs>(this, "KeyPress")
+                .Select(p => p.EventArgs.ToKeyPressNotification());
+
+            var keyboardStream = keyDowns
+                .Merge<KeyNotification>(keyUps)
+                .Merge(keyPresses);
+
+            keyboardEventOut[0] = new Keyboard(keyboardStream);
+        }
+
+        private void CreateTouchEventsOut()
+        {
+            var oa = new OutputAttribute("Touch Events");
+            oa.IsSingle = true;
+            var touchEventOut = FIOFactory.CreatePin<TouchDevice>(oa);
+
+            var touchDowns = Observable.FromEventPattern<WMTouchEventArgs>(this, "TouchDown")
+                .Select(p => p.EventArgs.ToTouchDownNotification(this));
+            var touchMoves = Observable.FromEventPattern<WMTouchEventArgs>(this, "TouchMove")
+                .Select(p => p.EventArgs.ToTouchMoveNotification(this));
+            var touchUps = Observable.FromEventPattern<WMTouchEventArgs>(this, "TouchUp")
+                .Select(p => p.EventArgs.ToTouchUpNotification(this));
+
+            var touchStream = touchDowns
+                .Merge<TouchNotification>(touchMoves)
+                .Merge(touchUps);
+
+            touchEventOut[0] = new TouchDevice(touchStream);
+        }
+    }
+
+    public static class EventExtensions
+    {
+        //mouse
+        public static MouseMoveNotification ToMouseMoveNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseMoveNotification(args.Location, relativeTo.ClientSize);
+        }
+
+        public static MouseDownNotification ToMouseDownNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseDownNotification(args.Location, relativeTo.ClientSize, args.Button);
+        }
+
+        public static MouseUpNotification ToMouseUpNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseUpNotification(args.Location, relativeTo.ClientSize, args.Button);
+        }
+
+        public static MouseClickNotification ToMouseClickNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseClickNotification(args.Location, relativeTo.ClientSize, args.Button, 1);
+        }
+
+        public static MouseClickNotification ToMouseDoubleClickNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseClickNotification(args.Location, relativeTo.ClientSize, args.Button, 2);
+        }
+
+        public static MouseWheelNotification ToMouseWheelNotification(this FormsMouseEventArgs args, Control relativeTo)
+        {
+            return new MouseWheelNotification(args.Location, relativeTo.ClientSize, args.Delta);
+        }
+
+        //keyboard
+        public static KeyDownNotification ToKeyDownNotification(this KeyEventArgs eventArgs)
+        {
+            return new KeyDownNotification(eventArgs.KeyCode);
+        }
+
+        public static KeyUpNotification ToKeyUpNotification(this KeyEventArgs eventArgs)
+        {
+            return new KeyUpNotification(eventArgs.KeyCode);
+        }
+
+        public static KeyPressNotification ToKeyPressNotification(this KeyPressEventArgs eventArgs)
+        {
+            return new KeyPressNotification(eventArgs.KeyChar);
+        }
+
+        //touch
+
+        enum TouchNotificationAge { Older, Intermediate, Newer};
+        static TouchNotificationAge TouchAge;
+
+        static ConstructorInfo STouchDownNotificationConstructor;
+        static ConstructorInfo STouchMoveNotificationConstructor;
+        static ConstructorInfo STouchUpNotificationConstructor;
+
+        public static TouchDownNotification ToTouchDownNotification(this WMTouchEventArgs eventArgs, Control relativeTo)
+        {
+            if (STouchDownNotificationConstructor == null) //find correct constructor
+            {
+                var touchNotificationType = typeof(TouchDownNotification);
+
+                var newerParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size), typeof(long) };
+                var newerConstructor = touchNotificationType.GetConstructor(newerParams);
+
+                if (newerConstructor != null)
+                {
+                    STouchDownNotificationConstructor = newerConstructor;
+                    TouchAge = TouchNotificationAge.Newer;
+                }
+                else
+                {
+                    var intermediateParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size) };
+                    var intermediateConstructor = touchNotificationType.GetConstructor(intermediateParams);
+
+                    if (intermediateConstructor != null)
+                    {
+                        STouchDownNotificationConstructor = intermediateConstructor;
+                        TouchAge = TouchNotificationAge.Intermediate;
+                    }
+                    else
+                    {
+                        var olderParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(Size) };
+                        var olderConstructor = touchNotificationType.GetConstructor(olderParams);
+
+                        if (olderConstructor != null)
+                        {
+                            STouchDownNotificationConstructor = olderConstructor;
+                            TouchAge = TouchNotificationAge.Older;
+                        }   
+                    }
+                }
+            }
+
+            switch (TouchAge)
+            {
+                case TouchNotificationAge.Newer:
+                    return (TouchDownNotification)STouchDownNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea(), eventArgs.TouchDeviceID });
+
+                case TouchNotificationAge.Intermediate:
+                    return (TouchDownNotification)STouchDownNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea() });
+
+                case TouchNotificationAge.Older:
+                    return (TouchDownNotification)STouchDownNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.ContactArea() });
+            }
+
+            return null;
+        }
+
+
+        public static TouchMoveNotification ToTouchMoveNotification(this WMTouchEventArgs eventArgs, Control relativeTo)
+        {
+            if (STouchMoveNotificationConstructor == null) //find correct constructor
+            {
+                var touchNotificationType = typeof(TouchMoveNotification);
+
+                var newerParams = new Type[] {typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size), typeof(long) };
+                var newerConstructor = touchNotificationType.GetConstructor(newerParams);
+
+                if (newerConstructor != null)
+                {
+                    STouchMoveNotificationConstructor = newerConstructor;
+                    TouchAge = TouchNotificationAge.Newer;
+                }
+                else
+                {
+                    var intermediateParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size) };
+                    var intermediateConstructor = touchNotificationType.GetConstructor(intermediateParams);
+
+                    if (intermediateConstructor != null)
+                    {
+                        STouchMoveNotificationConstructor = intermediateConstructor;
+                        TouchAge = TouchNotificationAge.Intermediate;
+                    }
+                    else
+                    {
+                        var olderParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(Size) };
+                        var olderConstructor = touchNotificationType.GetConstructor(olderParams);
+
+                        if (olderConstructor != null)
+                        {
+                            STouchMoveNotificationConstructor = olderConstructor;
+                            TouchAge = TouchNotificationAge.Older;
+                        }
+                    }
+                }
+            }
+
+            switch (TouchAge)
+            {
+                case TouchNotificationAge.Newer:
+                    return (TouchMoveNotification)STouchMoveNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea(), eventArgs.TouchDeviceID });
+
+                case TouchNotificationAge.Intermediate:
+                    return (TouchMoveNotification)STouchMoveNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea() });
+
+                case TouchNotificationAge.Older:
+                    return (TouchMoveNotification)STouchMoveNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.ContactArea() });
+            }
+
+            return null;
+        }
+
+        public static TouchUpNotification ToTouchUpNotification(this WMTouchEventArgs eventArgs, Control relativeTo)
+        {
+            if (STouchUpNotificationConstructor == null) //find correct constructor
+            {
+                var touchNotificationType = typeof(TouchUpNotification);
+
+                var newerParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size), typeof(long) };
+                var newerConstructor = touchNotificationType.GetConstructor(newerParams);
+
+                if (newerConstructor != null)
+                {
+                    STouchUpNotificationConstructor = newerConstructor;
+                    TouchAge = TouchNotificationAge.Newer;
+                }
+                else
+                {
+                    var intermediateParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(bool), typeof(Size) };
+                    var intermediateConstructor = touchNotificationType.GetConstructor(intermediateParams);
+
+                    if (intermediateConstructor != null)
+                    {
+                        STouchUpNotificationConstructor = intermediateConstructor;
+                        TouchAge = TouchNotificationAge.Intermediate;
+                    }
+                    else
+                    {
+                        var olderParams = new Type[] { typeof(Point), typeof(Size), typeof(int), typeof(Size) };
+                        var olderConstructor = touchNotificationType.GetConstructor(olderParams);
+
+                        if (olderConstructor != null)
+                        {
+                            STouchUpNotificationConstructor = olderConstructor;
+                            TouchAge = TouchNotificationAge.Older;
+                        }
+                    }
+                }
+            }
+
+            switch (TouchAge)
+            {
+                case TouchNotificationAge.Newer:
+                    return (TouchUpNotification)STouchUpNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea(), eventArgs.TouchDeviceID });
+
+                case TouchNotificationAge.Intermediate:
+                    return (TouchUpNotification)STouchUpNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.IsPrimaryContact, eventArgs.ContactArea() });
+
+                case TouchNotificationAge.Older:
+                    return (TouchUpNotification)STouchUpNotificationConstructor.Invoke(new object[] { eventArgs.Location(), relativeTo.ClientSize, eventArgs.Id, eventArgs.ContactArea() });
+            }
+
+            return null;
+        }
+
+        public static Point Location(this WMTouchEventArgs args)
+        {
+            return new Point(args.LocationX, args.LocationY);
+        }
+
+        public static Size ContactArea(this WMTouchEventArgs args)
+        {
+            return new Size(args.ContactX, args.ContactY);
         }
     }
 }
