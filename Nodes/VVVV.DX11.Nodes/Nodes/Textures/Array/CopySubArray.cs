5a9e5f1def8f041e980fe81c18b10ae42e6158c1
sebl <sebl@schnellebuntebilder.de>
2018-04-17 14:43:19 +0200

    init for new Node
    should work like the Getslice (TextureArray) but Outputs also a Texturearray instead of a spread of Textures)
    
    this commit is completely untested

---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/CopySubArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using FeralTic.Resources.Geometry;
+using SlimDX.Direct3D11;
+using SlimDX.DXGI;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+using VVVV.Utils.VMath;
+
+namespace VVVV.DX11.Nodes
+{
+    public class CopySubArray : IDX11Resource, IDisposable
+    {
+        private DX11RenderContext context;
+
+        private DX11RenderTextureArray rtarr;
+        public DX11RenderTextureArray Result { get { return rtarr; } }
+
+
+        public CopySubArray(DX11RenderContext context)
+        {
+            this.context = context;
+        }
+
+        public void Reset(DX11Texture2D texture, int w, int h, int d, SlimDX.DXGI.Format format)
+        {
+            format = format == SlimDX.DXGI.Format.Unknown ? texture.Format : format;
+            this.rtarr.Dispose();
+            this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, format, true, 1);
+        }
+
+        public void Apply(DX11Resource<DX11RenderTextureArray> textureArray, ISpread<int> slices)
+        {
+            int w = textureArray[context].Width;
+            int h = textureArray[context].Height;
+            int d = slices.SliceCount;
+            Format f = textureArray[context].Format;
+
+            Texture2DDescription descIn = textureArray[context].Resource.Description;
+
+            // check if parameters match - if not, create a new rt array
+            if (this.rtarr != null)
+            {
+                if (this.rtarr.ElemCnt != slices.SliceCount || 
+                    this.rtarr.Width != w || 
+                    this.rtarr.Height != h || 
+                    this.rtarr.Format != f)
+                {
+                    this.rtarr.Dispose(); this.rtarr = null;
+                }
+            }
+
+            if (this.rtarr == null)
+            {
+                this.rtarr = new DX11RenderTextureArray(this.context, w, h, d, f, true, 1);
+            }
+
+            // copy the ressources over
+            for (int i = 0; i < slices.SliceCount; i++)
+            {
+                int slice = VMath.Zmod(slices[i], textureArray[context].ElemCnt);
+
+                SlimDX.Direct3D11.Resource source = textureArray[context].Resource;
+
+                int sourceSubres = SlimDX.Direct3D11.Texture2D.CalculateSubresourceIndex(0, slice, descIn.MipLevels);
+
+                int destinationSubres = SlimDX.Direct3D11.Texture2D.CalculateSubresourceIndex(0, 0, 1);
+
+
+                context.CurrentDeviceContext.CopySubresourceRegion(source, sourceSubres, this.rtarr.Resource, destinationSubres, 0, 0, 0);
+
+            }
+            
+        }
+
+       
+        public void Dispose()
+        {
+            if (this.rtarr != null) { this.rtarr.Dispose(); }
+        }
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Textures/Array/CopySubArrayNode.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.Composition;
+
+
+using SlimDX;
+using SlimDX.Direct3D11;
+//using SlimDX.DXGI;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+
+using FeralTic.DX11.Geometry;
+using FeralTic.DX11.Resources;
+using FeralTic.DX11;
+using VVVV.DX11.Nodes;
+using VVVV.DX11;
+using VVVV.DX11.Lib.Rendering;
+
+using VVVV.Core.Logging;
+using VVVV.Utils.VMath;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "GetArray", Category = "DX11.TextureArray", Version = "", Author = "sebl")]
+    public class CopySubArrayNode : IPluginEvaluate, IDX11ResourceHost, IDisposable
+    {
+        [Input("TextureArray In", IsSingle = true)]
+        protected Pin<DX11Resource<DX11RenderTextureArray>> FTexIn;
+
+        [Input("Index")]
+        protected IDiffSpread<int> FIndex;
+
+        [Output("Textures Out")]
+        protected ISpread<DX11Resource<DX11RenderTextureArray>> FTextureOutput;
+
+        private DX11Resource<CopySubArray> generators = new DX11Resource<CopySubArray>();
+
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FTextureOutput[0] == null)
+            {
+                this.FTextureOutput[0] = new DX11Resource<DX11RenderTextureArray>();
+            }
+        }
+
+        public void Update(DX11RenderContext context)
+        {
+            if (!this.generators.Contains(context))
+            {
+                this.generators[context] = new CopySubArray(context);
+            }
+
+            if (this.FTexIn.IsConnected)
+            {
+                var generator = this.generators[context];
+
+                generator.Apply(this.FTexIn[0], this.FIndex);
+                this.WriteResult(generator, context);
+
+            }
+        }
+
+        private void WriteResult(CopySubArray generator, DX11RenderContext context)
+        {
+            DX11RenderTextureArray result = generator.Result;
+            this.FTextureOutput[0][context] = generator.Result;
+        }
+
+        public void Destroy(DX11RenderContext context, bool force)
+        {
+            if (this.generators != null && this.generators.Contains(context))
+            {
+                this.generators.Dispose(context);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.generators != null)
+            {
+                this.generators.Dispose();
+                this.generators = null;
+            }
+
+        }
+    }
+}
