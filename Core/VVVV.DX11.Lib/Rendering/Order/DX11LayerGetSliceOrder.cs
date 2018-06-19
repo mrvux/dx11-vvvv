2c064685242f35b0c0f984060a3b2dec17e708ff
Julien Vulliet <julien@mrvux.com>
2018-06-19 20:06:47 +0200

    Move some order presets ijn lib

---
+++ Core/VVVV.DX11.Lib/Rendering/Order/DX11LayerGetSliceOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.DX11.Lib
+{
+    public class DX11LayerGetSliceOrder : IDX11LayerOrder
+    {
+        private List<int> internalBuffer = new List<int>();
+
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+
+        public ISpread<int> FInIndex { get; set; }
+
+        public List<int> Reorder(DX11RenderSettings settings, List<DX11ObjectRenderSettings> objectSettings)
+        {
+            internalBuffer.Clear();
+            for (int i = 0; i < FInIndex.SliceCount; i++)
+            {
+                internalBuffer.Add(FInIndex[i]);
+            }
+            return this.internalBuffer;
+        }
+    }
+}
---
+++ Core/VVVV.DX11.Lib/Rendering/Order/DX11SingleSliceOrder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVVV.DX11.Lib
+{
+    public class DX11SingleSliceOrder : IDX11LayerOrder
+    {
+        private List<int> internalBuffer = new List<int>();
+
+        public DX11SingleSliceOrder()
+        {
+            this.Enabled = true;
+        }
+
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+
+        public int FInIndex;
+
+        public List<int> Reorder(DX11RenderSettings settings, List<DX11ObjectRenderSettings> objectSettings)
+        {
+            internalBuffer.Clear();
+            internalBuffer.Add(this.FInIndex);
+            return this.internalBuffer;
+        }
+    }
+}
