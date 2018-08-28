71f6b973094fd08675116f36f5b4a7ca1a72789f
velcrome <marko@intolight.de>
2018-08-28 17:34:45 +0200

    New WithinViewportIndexNode to manually pick a viewport for a given layer

---
+++ Core/VVVV.DX11.Lib/Rendering/Validators/DX11WithinViewportValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace VVVV.DX11.Validators
+{
+    public class DX11WithinViewportValidator : IDX11ObjectValidator
+    {
+        private DX11RenderSettings settings;
+
+        public bool Enabled { get; set; }
+
+        public int ViewPortIndex { get; set; }
+
+        public void SetGlobalSettings(DX11RenderSettings settings)
+        {
+            this.settings = settings;
+
+        }
+
+        public bool Validate(DX11ObjectRenderSettings obj)
+        {
+            return ViewPortIndex == settings.ViewportIndex;
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
---
+++ Nodes/VVVV.DX11.Nodes/Nodes/Validators/WithinViewportValidatorNode.cs
@@ -0,0 +1,26 @@
+﻿using VVVV.DX11.Validators;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.DX11.Nodes
+{
+    [PluginInfo(Name = "WithinViewportIndex", Category = "DX11.Validator", Version = "", Author = "velcrome", Tags = "layer")]
+    public class WithinViewportIndexNode : IPluginEvaluate
+    {
+        [Input("Enabled", DefaultValue = 1)]
+        protected ISpread<bool> FInEnabled;
+
+        [Input("Viewport Index", DefaultValue = 0, IsSingle = true)]
+        protected ISpread<int> FViewportIndex;
+
+        [Output("Output", IsSingle = true)]
+        protected ISpread<DX11WithinViewportValidator> FOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (this.FOut[0] == null) { this.FOut[0] = new DX11WithinViewportValidator(); }
+            this.FOut[0].Enabled = this.FInEnabled[0];
+            this.FOut[0].ViewPortIndex = this.FViewportIndex[0];
+            this.FOut[0].Reset();
+        }
+    }
+}
