b5ba3900f90c6c4448805705dbedeb70f0fd4839
Chris <chris@wirmachenbunt.de>
2019-05-10 10:53:48 +0200

    setcustomnode

---
+++ Nodes/VVVV.DX11.Nodes.Bullet/Nodes/Bodies/Interactions/Rigid/BulletSetCustomNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VVVV.PluginInterfaces.V2;
+using BulletSharp;
+using VVVV.Utils.VMath;
+using VVVV.Bullet.Core;
+
+namespace VVVV.Bullet.Nodes.Bodies.Interactions.Rigid
+{
+    [PluginInfo(Name = "SetCustom", Category = "Bullet", Version = "Rigid", Author = "vux",
+        Help = "Updates rigid body custom string", AutoEvaluate = true)]
+    public class BulletSetCustomNode : IPluginEvaluate
+    {
+        [Input("Bodies", Order = 0)]
+        protected ISpread<RigidBody> FInput;
+
+        
+
+        [Input("Custom String")]
+        protected ISpread<string> FString;
+
+        [Input("Set")]
+        protected IDiffSpread<bool> FSet;
+
+
+        public void Evaluate(int SpreadMax)
+        {
+            for (int i = 0; i < SpreadMax; i++)
+            {
+                RigidBody rb = this.FInput[i];
+                
+
+                if (rb != null && FSet.IsChanged && FSet[i])
+                {
+
+                    BodyCustomData bd = (BodyCustomData)rb.UserObject;
+                    bd.Custom = FString[i];  
+                    
+                    
+                }
+            }
+        }
+    }
+}
